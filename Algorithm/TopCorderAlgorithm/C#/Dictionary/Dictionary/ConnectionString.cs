9049f4e9ac4e924fa935919170b4ba29cff2e4ef
JungSeongCheol <kir5121@naver.com>
2021-03-05 22:49:51 +0900

    Connection String C# 추가

---
+++ Algorithm/TopCorderAlgorithm/C#/Dictionary/Dictionary/ConnectionString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    class ConnectionString
+    {
+        static void countString(string[] s)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dic.ContainsKey(s[i])) dic[s[i]] = 0;
+                dic[s[i]]++;
+            }
+
+            foreach (KeyValuePair<string, int> item in dic)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] MyString = { "Hello", "My", "String", "Hello" };
+            countString(MyString);
+        }
+    }
+}
