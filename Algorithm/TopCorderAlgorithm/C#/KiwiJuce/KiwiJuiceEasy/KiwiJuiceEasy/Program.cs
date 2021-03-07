5bc7335c543984123f89041287644da2cf0d06b4
JungSeongCheol <kir5121@naver.com>
2021-03-07 23:05:25 +0900

    Example1

---
+++ Algorithm/TopCorderAlgorithm/C#/KiwiJuce/KiwiJuiceEasy/KiwiJuiceEasy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KiwiJuiceEasy
+{
+    class KiwiJuiceEasy
+    {
+        public int[] thePouring(int[] capacities, int[] bottles, int[] fromId, int[] toId)
+        {
+            int[] aa = null;
+            return aa;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
