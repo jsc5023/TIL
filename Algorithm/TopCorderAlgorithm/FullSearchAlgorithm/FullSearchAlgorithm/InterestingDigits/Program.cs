97eacd23586a9a4eb479fd1905d3f5335af208cb
JungSeongCheol <kir5121@naver.com>
2021-03-18 22:48:20 +0900

    Ineresting Digits 코드 작성

---
+++ Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/InterestingDigits/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterestingDigits
+{
+    class InterestingDigits
+    {
+        
+        public static int[] digits(int bas)
+        {
+            int[] ans = new int[bas - 2]; // 임시로 리턴값을 지정, 값을 어떻게 구해야 할 지 잘 감이 잡히지 않음
+            for (int i = 2  ; i < bas; i++)
+            {
+
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            // Example 1
+            // base = 10;
+            // Return : {3, 9}
+
+            // Example 2
+            // base = 3;
+            // Return : {2}
+
+            // Example
+            // base = 9;
+            // Return : {5, 25}
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
