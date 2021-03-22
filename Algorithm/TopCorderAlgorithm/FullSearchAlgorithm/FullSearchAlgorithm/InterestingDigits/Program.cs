7d8b62877c313817f6157e281fad9ce7062b7554
JungSeongCheol <kir5121@naver.com>
2021-03-22 22:45:18 +0900

    일반적인 전체 탐색(interestingDigits) 풀이 방법

--- Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/InterestingDigits/Program.cs
+++ Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/InterestingDigits/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterestingDigits
 {
@@ -15,6 +16,35 @@ namespace InterestingDigits
                
             }
             return ans;
+        }
+
+        public static int[] digitsBasicSolution(int bas)
+        {
+            List<int> ans = new List<int>();
+
+            for (int i = 2; i < bas; i++)
+            {
+                bool ok = true;
+                for (int k1 = 0; k1 < bas; k1++)
+                {
+                    for (int k2 = 0; k2 < bas; k2++)
+                    {
+                        for (int k3 = 0; k3 < bas; k3++)
+                        {
+                            if((k1 + k2 * bas + k3 * bas * bas) % i == 0 && (k1+k2+k3) % i != 0)
+                            {
+                                // n의 배수 숫자로 각 숫자의 합이 n의 배수가 아니면 n을 제외합니다.
+                                ok = false;
+                                break;
+                            }
+                            if (!ok) break;
+                        }
+                        if (ok) break;
+                    }
+                    if (ok) break;
+                }
+            }
+            return ans.ToArray();
         }
 
         static void Main(string[] args)
