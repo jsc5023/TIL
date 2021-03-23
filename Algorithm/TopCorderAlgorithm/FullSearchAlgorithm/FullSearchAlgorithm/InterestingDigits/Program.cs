7e2bf95fd1eb72b8cb35d76e8bf22561f621a82d
JungSeongCheol <kir5121@naver.com>
2021-03-23 23:12:41 +0900

    재미있는 수학 직접 풀이(해설 참조)

--- Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/InterestingDigits/Program.cs
+++ Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/InterestingDigits/Program.cs
@@ -10,12 +10,41 @@ namespace InterestingDigits
             // 잘 감이 안잡혀서 문제 해설을 참고하여 푼 문제이다.
             // 핵심은 4자리 미만이라는 조건이다.
             // 3자리 까지만 고려하면 되므로, 1의 자리, 2의자리, 3의 자리 모두 확인해 보면된다.
-            int[] ans = new int[bas - 2]; 
-            for (int i = 2  ; i < bas; i++)
+
+            // 수정필요
+            List<int> ans = new List<int>();
+            for (int i = 2  ; i < bas; i++) // 현재 숫자( bas진수에서의 선택한 숫자 입니다.)
             {
-               
+                bool nowBreak = true;
+                // 현재 
+                for (int j = 0; j < bas; j++) // 첫번째 자리
+                {
+                    for (int k = 0; k < bas; k++) // 두번째 자리
+                    {
+                        for (int m = 0; m < bas; m++) // 세번째 자리
+                        {
+                            //if(((j + k + m) % i == 0 )&& ((j * k * m) % i == 0))
+                            //{
+                            //    ans.Add(j);
+                            //    ans.Add(k);
+                            //    ans.Add(m);
+                            //    nowBreak = true;
+                            //    break;
+                            //}
+
+                            if (((j + k + m) % i != 0) && ((j + k * bas + m * bas * bas) % i == 0)) // 진수의 자리수 표현(j + k * bas + m * bas * bas)
+                            {
+                                nowBreak = false;
+                                break;
+                            }
+                        }
+                        if (!nowBreak) break;
+                    }
+                    if (!nowBreak) break;
+                }
+                if (nowBreak == true) ans.Add(i);
             }
-            return ans;
+            return ans.ToArray();
         }
 
         public static int[] digitsBasicSolution(int bas)
@@ -61,7 +90,13 @@ namespace InterestingDigits
             // base = 9;
             // Return : {5, 25}
 
-            Console.WriteLine("Hello World!");
+            int bas = 10;
+
+            for (int i = 0; i < digits(bas).Length; i++)
+            {
+                Console.WriteLine(digits(bas)[i]);
+        }
+            
         }
     }
 }
