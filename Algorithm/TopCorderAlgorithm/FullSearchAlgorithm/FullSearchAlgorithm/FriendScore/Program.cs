e40e8f9c38485f34301f7c916fbf374091d03769
JungSeongCheol <kir5121@naver.com>
2021-04-01 23:27:52 +0900

    친구 수 문제 풀이방법 일부 작성

--- Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/FriendScore/Program.cs
+++ Algorithm/TopCorderAlgorithm/FullSearchAlgorithm/FullSearchAlgorithm/FriendScore/Program.cs
@@ -5,25 +5,39 @@ namespace FriendScore
     class FriendScore
     {
         // TODO
-        public int highestScore(string[] friends)
+        public static int highestScore(string[] friends)
         {
-
+            //일단 직접 친구만 구하는 수식을 구했음
+            int ans = 0;
+            int n = friends[0].Length; // 처음사람의 사람과의 관계를 잡는다.(직접 친구 찾기)
+            int cnt = 0;// 친구수
+            for (int i = 0; i < n; i++)
+            {
+                cnt = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (friends[i][j] == 'Y')
+                        cnt++;
+                }
+            }
+            ans = cnt;
+            return ans;
         }
 
         static void Main(string[] args)
         {
             // Example 1
-            // string[] friends = {"NNN", "NNN", "NNN" }
-            // Returns: 0
+            //string[] friends = { "NNN", "NNN", "NNN" };
+            //Returns: 0
 
             // Example 2
-            // string[] friends = {"NYY", "YNY", "YYN" }
+            string[] friends = { "NYY", "YNY", "YYN" };
             // Returns : 2
 
             // Example 3
             // string[] friends = {"NYNNN", "YNYNN", "NYNYN", "NNYNY", "NNNYN"}
             // Returns : 4
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(highestScore(friends));
         }
     }
 }
