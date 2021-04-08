9849e87514ef593133fc38e78517dc5fbb7caad1
JungSeongCheol <kir5121@naver.com>
2021-04-08 22:05:50 +0900

    고장난 로봇 문제 작성

---
+++ Algorithm/TopCorderAlgorithm/Graph/Graph/CrazyBot/CrazyBot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CrazyBot
+{
+    class CrazyBot
+    {
+        public double getProbability(int n, int east, int west, int south, int north)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Example 1
+            //int n = 1;
+            //int east = 25;
+            //int west = 25;
+            //int south = 25;
+            //int north = 25;
+            //return : 10
+
+            // Example 2
+            //int n = 2;
+            //int east = 25;
+            //int west = 25;
+            //int south = 25;
+            //int north = 25;
+            //return : 0.75;
+
+            // Example 3
+            //int n = 7;
+            //int east = 50;
+            //int west = 0;
+            //int south = 0;
+            //int north = 50;
+            //return 1.0;
+            //Console.WriteLine(getProbability(n, east, west, south, north);
+        }
+    }
+}
