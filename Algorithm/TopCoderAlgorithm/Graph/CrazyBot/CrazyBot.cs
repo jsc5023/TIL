92a082ad9b634a69701ce3d7f7a469e2ceaa0291
JungSeongCheol <kir5121@naver.com>
2021-04-15 23:51:43 +0900

    TopCoder 알고리즘 풀이(고장난 로봇, 아직 푸는중)

--- Algorithm/TopCoderAlgorithm/Graph/CrazyBot/CrazyBot.cs
+++ Algorithm/TopCoderAlgorithm/Graph/CrazyBot/CrazyBot.cs
@@ -4,9 +4,14 @@ namespace CrazyBot
 {
     class CrazyBot
     {
+        // 우선 책을 참조해서 풀어나갑니다.(깊이우선탐색을 이해하기 편하게 하기  위해
+        bool[,] grid = new bool[100, 100]; // 가변배열 [,]
+        int[] vx = new int[4] { 1, -1, 0, 0 };
+        
         public double getProbability(int n, int east, int west, int south, int north)
         {
-
+            // 기본적으로 모든 값을 찾아야 되고, 많은 스택을 사용하지는 않으므로 깊이우선탐색으로 문제를 풀어나갑니다.
+            
         }
 
         static void Main(string[] args)
@@ -20,11 +25,11 @@ namespace CrazyBot
             //return : 10
 
             // Example 2
-            //int n = 2;
-            //int east = 25;
-            //int west = 25;
-            //int south = 25;
-            //int north = 25;
+            int n = 2;
+            int east = 25;
+            int west = 25;
+            int south = 25;
+            int north = 25;
             //return : 0.75;
 
             // Example 3
