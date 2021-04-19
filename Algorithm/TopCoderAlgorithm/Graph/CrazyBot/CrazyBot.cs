8c56a805d980497cae62fe0e302e5d27d7c653b6
JungSeongCheol <kir5121@naver.com>
2021-04-19 23:22:01 +0900

    CrazyBot dfs 참조

--- Algorithm/TopCoderAlgorithm/Graph/CrazyBot/CrazyBot.cs
+++ Algorithm/TopCoderAlgorithm/Graph/CrazyBot/CrazyBot.cs
@@ -5,13 +5,38 @@ namespace CrazyBot
     class CrazyBot
     {
         // 우선 책을 참조해서 풀어나갑니다.(깊이우선탐색을 이해하기 편하게 하기  위해
-        bool[,] grid = new bool[100, 100]; // 가변배열 [,]
+        bool[,] grid = new bool[100, 100];
         int[] vx = new int[4] { 1, -1, 0, 0 };
-        
+        int[] vy = new int[4] { 0, 0, 1, -1 };
+        double[] prob = new double[4];
+
         public double getProbability(int n, int east, int west, int south, int north)
         {
             // 기본적으로 모든 값을 찾아야 되고, 많은 스택을 사용하지는 않으므로 깊이우선탐색으로 문제를 풀어나갑니다.
-            
+            prob[0] = east / 100.0;
+            prob[1] = west / 100.0;
+            prob[2] = south / 100.0;
+            prob[3] = north / 100.0;
+
+            return dfs(50, 50, n);
+        }
+
+        double dfs(int x, int y, int n)
+        {
+            if (grid[x, y]) return 0;
+            if (n == 0) return 1;
+
+            grid[x, y] = true;
+            double ret = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                // east, west, south, north 순서로 로봇을 움직입니다.
+                ret += dfs(x + vx[i], y + vy[i], n - 1) * prob[i];
+            }
+
+            grid[x, y] = false;
+
+            return ret;
         }
 
         static void Main(string[] args)
