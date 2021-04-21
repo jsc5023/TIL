660aebbc819d17b25e75b4370f7e46fd0c5379a3
JungSeongCheol <kir5121@naver.com>
2021-04-21 23:25:52 +0900

    MazeMaker(미로 만드는 사람) 예제 작성

---
+++ Algorithm/TopCoderAlgorithm/Graph/MazeMaker/MazeMaker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MazeMaker
+{
+    class MazeMaker
+    {
+        public int longestPath(string[] maze, int startRow, int startCol, int[] moveRow, int[] moveCol)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Example 1
+            //string[] maze = { "...", "...", "..." };
+            //int startRow = 0;
+            //int startCol = 1;
+            //int[] moveRow = { 1, 0, -1, 0 };
+            //int[] moveCol = { 0, 1, 0, -1 };
+            //return 3;
+
+            // Example 2
+            //string[] maze = { "...", "...", "..." };
+            //int startRow = 0;
+            //int startCol = 1;
+            //int[] moveRow = { 1, 0, -1, 0, 1, 1, -1, -1 };
+            //int[] moveCol = { 0, 1, 0, -1 , 1, -1, 1, -1};
+            //return 2
+
+            // Example 3
+            //string[] maze = { "X.X",
+            //                  "...",
+            //                  "XXX",
+            //                  "X.X"};
+            //int startRow = 0;
+            //int startCol = 1;
+            //int[] moveRow = { 1, 0, -1, 0 };
+            //int[] moveCol = { 0, 1, 0, -1};
+            //return -1;
+
+            // Example 2
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
