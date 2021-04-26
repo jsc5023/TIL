5abfd479b4e0e5dd67e1e2b5a4eeceea7236f65f
JungSeongCheol <kir5121@naver.com>
2021-04-26 23:55:44 +0900

    메이즈 메이커 푸는 방법 작성

--- Algorithm/TopCoderAlgorithm/Graph/MazeMaker/MazeMaker.cs
+++ Algorithm/TopCoderAlgorithm/Graph/MazeMaker/MazeMaker.cs
@@ -6,7 +6,9 @@ namespace MazeMaker
     {
         public int longestPath(string[] maze, int startRow, int startCol, int[] moveRow, int[] moveCol)
         {
-
+            //너비 우선탐색이 더 좋습니다.(왜냐하면 횟수가 너무 많기 떄문에 깊이우선탐색이 효율이 나오지않기떄문입니다.)
+            int max = 0;
+            int width = maze[0];
         }
 
         static void Main(string[] args)
