d27dc9c31032653ff5eb3e46058e3dbba84e4c2a
JungSeongCheol <kir5121@naver.com>
2021-04-27 23:36:14 +0900

    mazeMaker 코드작성 (미완성)

--- Algorithm/TopCoderAlgorithm/Graph/MazeMaker/MazeMaker.cs
+++ Algorithm/TopCoderAlgorithm/Graph/MazeMaker/MazeMaker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic; // Queue를 쓰기 위해 사용함
 
 namespace MazeMaker
 {
@@ -7,8 +8,20 @@ namespace MazeMaker
         public int longestPath(string[] maze, int startRow, int startCol, int[] moveRow, int[] moveCol)
         {
             //너비 우선탐색이 더 좋습니다.(왜냐하면 횟수가 너무 많기 떄문에 깊이우선탐색이 효율이 나오지않기떄문입니다.)
+            // 예제에 나온 코드를 참조합니다.(주석추가, 추후 새로 작성)
             int max = 0;
-            int width = maze[0];
+            int width = maze[0].Length, height = maze.Length;
+            int[,] board = new int[height, width];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    board[i, j] = -1;
+
+            board[startRow, startCol] = 0;
+
+            Queue<int> queueX = new Queue<int>();
+            Queue<int> queueY = new Queue<int>();
+
         }
 
         static void Main(string[] args)
