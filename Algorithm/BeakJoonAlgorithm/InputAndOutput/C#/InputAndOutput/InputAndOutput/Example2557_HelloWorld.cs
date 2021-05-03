8ab5422d1c323a2ffc4e918cd5dd7ae5b6c0d708
JungSeongCheol <kir5121@naver.com>
2021-05-04 07:22:21 +0900

    c# 백준 2557 문제

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/InputAndOutput/Example2557_HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example2557_HelloWorld
+{
+    class Example2557_HelloWorld // 백준에서는 class 이름이 Program이여야 합니다.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
