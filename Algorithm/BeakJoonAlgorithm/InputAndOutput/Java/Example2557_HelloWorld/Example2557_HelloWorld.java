5932a65c733670239d54b67556e65a9ce93e2bb3
JungSeongCheol <kir5121@naver.com>
2021-05-04 06:34:14 +0900

    백준알고리즘 입출력문제 자바코드 추가

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example2557_HelloWorld/Example2557_HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class Example2557_HelloWorld {
+
+	public static void main(String[] args) { // 실제 프로그램 Class 명칭은 Main으로 사용해야 됩니다.
+		System.out.println("Hello World!");
+	}
+
+}
--- Algorithm/BeakJoonAlgorithm/README.md
+++
@@ -1,4 +0,0 @@
-# HelloCodingAlogrithm
-
-백준 알고리즘을 풀었던 코드를 저장하는 장소입니다.
-Java 또는 C#위주로 업로드됩니다.
