fe3f0f38e8e28ba3e7274ab3353854bd45773ed8
JungSeongCheol <kir5121@naver.com>
2021-05-05 07:03:29 +0900

    C#, JAVA 백준 1000번 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example1000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            Console.WriteLine(a+b);
+        }
+    }
+}
