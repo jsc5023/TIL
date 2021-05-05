dc8ba23f3206ca93bf66ca38b3e84034c899bfb4
JungSeongCheol <kir5121@naver.com>
2021-05-06 07:12:58 +0900

    2558 문제 풀이 완료

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2558/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// C#에서는 1000과 다른점이, 줄이 바뀐것이랑 띄워쓰기를 한것의 차이는 Split을 해야하나, 안해야하나의 차이가 있다. 따라서 이렇게 써야된다.
+namespace Example2558
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example2558/src/Example2558.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// 기본적으로 1000과 같은 문제(줄이 바뀌냐 안바뀌냐의 차이므로 같이 풀어도된다.)
+public class Example2558 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+	}
+}
