137b12f78c23c3d416326e0015f929bd15e5cda4
JungSeongCheol <kir5121@naver.com>
2021-05-08 23:10:04 +0900

    C# 10850 문제

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10950/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Example10950
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
