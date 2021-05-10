9900d271e74bd2b0344d2f35a28532932aa6de51
JungSeongCheol <kir5121@naver.com>
2021-05-10 22:00:04 +0900

    C#으로 문제푼 10951번 문제

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10951/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Example10951
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // EOF 사용해야됩니다.
+            {
+                string input = Console.ReadLine();
+                if (input == null) break;
+                string[] s = input.Split();
+
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
