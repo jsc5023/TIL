7ec40b72ae2d0d3fce5ece422ec944084e29f45e
JungSeongCheol <kir5121@naver.com>
2021-05-11 19:23:44 +0900

    10953문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10953/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example10953
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tstring = Console.ReadLine();
+
+            int t = int.Parse(tstring);
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(",");
+
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
