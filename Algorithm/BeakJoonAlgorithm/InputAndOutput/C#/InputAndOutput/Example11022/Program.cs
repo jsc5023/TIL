bc9aa48b99544c2136882de22afbda8b7ab08c6b
JungSeongCheol <kir5121@naver.com>
2021-05-13 22:36:06 +0900

    Java 11718번 문제

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example11022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example11022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= t; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                int sum = a + b;
+
+                Console.WriteLine("Case #" + i + ": " + a + " + " + b + " = " + sum);
+            }
+        }
+    }
+}
---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example11718/Example11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Example11718 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextLine()) {
+			String input = scan.nextLine();
+			
+			System.out.println(input);
+		}
+	}
+}
