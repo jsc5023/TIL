07f91f9baf633e317c0dbd78e3fab93497731740
JungSeongCheol <kir5121@naver.com>
2021-05-13 22:41:18 +0900

    11718 C# 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example11718/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example11718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == null) break;
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
