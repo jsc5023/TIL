971152b943399e0a6956e8d1b4b26a00c9f1ef57
JungSeongCheol <kir5121@naver.com>
2021-05-18 07:20:20 +0900

    11720 Java 문제 푸는중(미완성)

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example11720/Example11720.java
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example11720/Example11720.java
@@ -4,14 +4,19 @@ public class Example11720 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n = scan.nextInt();
+		double n = scan.nextDouble();
 		
 		int sum = 0;
+		
+		int a = scan.nextInt();
+//		a = (int) (a % (Math.pow(10.0, n-1)));
 		for(int i=0; i<n; i++) {
-			int a = scan.nextInt();
-			
+			int b = a / (int)(Math.pow(10.0, n-1));
+			a = (int) (a % (Math.pow(10.0, n-1)));
+			sum += b;
 		}
 		
 		System.out.println(sum);
+		scan.close();
 	}
 }
