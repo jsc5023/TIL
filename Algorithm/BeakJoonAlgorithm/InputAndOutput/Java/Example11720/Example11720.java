0fd8f13d4795b412536a8c546b32aa8f04807f58
JungSeongCheol <kir5121@naver.com>
2021-05-17 23:35:33 +0900

    Java 11720 문제풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example11720/Example11720.java
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example11720/Example11720.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Example11720 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			int a = scan.nextInt();
+			
+		}
+		
+		System.out.println(sum);
+	}
 }
