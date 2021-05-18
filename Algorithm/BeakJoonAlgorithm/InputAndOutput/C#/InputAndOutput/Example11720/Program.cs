95e25283c9231bc04412acd3a220948033761618
JungSeongCheol <kir5121@naver.com>
2021-05-19 00:15:16 +0900

    C# 11720 문제 풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example11720/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example11720/Program.cs
@@ -6,7 +6,17 @@ namespace Example11720
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            string a = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+            {
+                //Console.WriteLine(a.Substring(i, 1));
+                sum += int.Parse(a.Substring(i, 1));
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
