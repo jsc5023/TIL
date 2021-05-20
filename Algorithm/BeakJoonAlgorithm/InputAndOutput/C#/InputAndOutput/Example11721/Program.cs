7d532427e8e49785d1417037a20178b0a1fde44f
JungSeongCheol <kir5121@naver.com>
2021-05-21 00:41:11 +0900

    C# Example 11721 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example11721/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example11721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            while (true)
+            {
+                if(a.Length >= 10)
+                {
+                    Console.WriteLine(a.Substring(0, 10));
+                }
+                else
+                {
+                    Console.WriteLine(a.Substring(0, a.Length));
+                    break;
+                }
+
+                a = a.Substring(10, a.Length - 10);
+            }
+        }
+    }
+}
