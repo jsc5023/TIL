c8f25aa7e41b1d9a260e39accb8d9b1be47755df
JungSeongCheol <kir5121@naver.com>
2021-05-21 07:03:49 +0900

    C# 2741번 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2741/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Example2741
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder allnumbers = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                allnumbers.Append(i + "\n"); // 하나씩 Console.WriteLine(i) 로 찍어버리면 속도 오류가 난다. 그래서 StringBuilder를 이용하는게 Best
+            }
+            Console.Write(allnumbers);
+        }
+    }
+}
