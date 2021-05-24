f34765475ddf63a0c0fbff7dbc9c82758675c724
JungSeongCheol <kir5121@naver.com>
2021-05-25 07:26:28 +0900

    C# 2742 문제

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2742/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+namespace Example2742
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = n; i > 0; i--)
+            {
+                sb.Append(i + "\n");
+            }
+            Console.Write(sb);
+        }
+    }
+}
