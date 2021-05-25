64b5ba2ccdf0bd4d1ec00de71411eec4d187dbbf
JungSeongCheol <kir5121@naver.com>
2021-05-26 00:04:18 +0900

    C# 2739 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2739/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example2739
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+
+                Console.WriteLine(n + " * " + i + " = " + n * i);
+            }
+        }
+    }
+}
---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/InputAndOutput/Example2739.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example2557_HelloWorld
+{
+    class Example2739
+    {
+    }
+}
