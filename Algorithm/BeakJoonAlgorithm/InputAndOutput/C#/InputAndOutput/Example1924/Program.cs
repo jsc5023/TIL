c81ae392dc33b662702f89c956a492e8fff4802f
JungSeongCheol <kir5121@naver.com>
2021-05-28 07:24:09 +0900

    c# 1924 풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example1924/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example1924/Program.cs
@@ -37,14 +37,45 @@ namespace Example1924
 
             return nowDayString;
         }
+
+        public static int MonthToDay(int month)
+        {
+            int day = 0;
+            
+            if (month > 1)
+                day += 31;
+            if (month > 2)
+                day += 28;
+            if (month > 3)
+                day += 31;
+            if (month > 4)
+                day += 30;
+            if (month > 5)
+                day += 31;
+            if (month > 6)
+                day += 30;
+            if (month > 7)
+                day += 31;
+            if (month > 8)
+                day += 31;
+            if (month > 9)
+                day += 30;
+            if (month > 10)
+                day += 31;
+            if (month > 11)
+                day += 30;
+
+            return day;
+        }
+
         static void Main(string[] args)
         {
             string[] input = Console.ReadLine().Split();
 
             int x = int.Parse(input[0]);
             int y = int.Parse(input[1]);
-
-            int nowDay = y % 7;
+            
+            int nowDay = (MonthToDay(x) + y) % 7;
 
             string nowDayString = FindDayString(nowDay);
 
