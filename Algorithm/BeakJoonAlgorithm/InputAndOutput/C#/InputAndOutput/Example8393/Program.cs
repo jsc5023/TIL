0fbc0b7e09a5f9e63a895fc3c01be4e793a37d70
JungSeongCheol <kir5121@naver.com>
2021-06-03 07:18:42 +0900

    C# Example 8393 폴더 생성

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example8393/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example8393
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
