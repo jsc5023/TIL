74eba1dfa69f0e556ccd737e30c2c36c687c6976
JungSeongCheol <kir5121@naver.com>
2021-06-04 23:34:05 +0900

    8393 C# 풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example8393/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example8393/Program.cs
@@ -6,7 +6,14 @@ namespace Example8393
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
