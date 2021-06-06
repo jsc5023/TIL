c340d7a77f7863ab38b04bce962d60fdd3bbdc68
JungSeongCheol <kir5121@naver.com>
2021-06-06 23:27:16 +0900

    10818 C# 문제 생성

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10818/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example10818
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
