44d89615e5bbb884c5715823613a22501384a665
JungSeongCheol <kir5121@naver.com>
2021-06-07 23:49:37 +0900

    C# 10818 미완성

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10818/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10818/Program.cs
@@ -6,6 +6,15 @@ namespace Example10818
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            int max = 0;
+            int min = 1000001;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+            }
             Console.WriteLine("Hello World!");
         }
     }
