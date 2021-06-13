e0ee44be70e72926d2701e72a600ea4d67cc9650
JungSeongCheol <kir5121@naver.com>
2021-06-13 22:16:56 +0900

    C# 2438 문제풀이 완료

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2438/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2438/Program.cs
@@ -6,7 +6,16 @@ namespace Example2438
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
