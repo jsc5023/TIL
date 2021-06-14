93716c7f09e0b1d6c57c6938ef5eae236efe401a
JungSeongCheol <kir5121@naver.com>
2021-06-14 23:06:49 +0900

    C# Example2439

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2439/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example2439
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = i - 1; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
