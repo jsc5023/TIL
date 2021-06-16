562ef9d6f7c798007d642c4f401c43d103721158
JungSeongCheol <kir5121@naver.com>
2021-06-16 22:59:55 +0900

    2440 C# 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2440/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example2440
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
