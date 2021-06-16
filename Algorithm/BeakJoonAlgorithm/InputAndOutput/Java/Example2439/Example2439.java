fd7a38061e3c2dc73dc239afa09f6835330b8ee8
JungSeongCheol <kir5121@naver.com>
2021-06-16 22:49:21 +0900

    2439 Java 풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example2439/Example2439.java
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example2439/Example2439.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Example2439 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			
+			for(int j = n - 1; j > i; j--) {
+				System.out.print(" ");
+			}
+			
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.print("\n");
+		}
+	}
 }
