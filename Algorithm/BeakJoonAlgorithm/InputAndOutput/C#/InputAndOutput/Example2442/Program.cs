be98c5f7b2785dd7c885bdc734bc1fdc5db09044
JungSeongCheol <kir5121@naver.com>
2021-06-23 23:22:56 +0900

    C#2442 최종 풀이

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2442/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2442/Program.cs
@@ -9,15 +9,21 @@ namespace Example2442
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                for (int j = n; j > 0; j--)
+                for (int j = n - 1; j > i; j--)
                 {
                     Console.Write(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
                 }
 
                 for (int j = 0; j < i; j++)
                 {
                     Console.Write("*");
                 }
+                Console.Write("\n");
             }
         }
     }
