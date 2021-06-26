8c1001ca30480603157fc9527909f22b87b5f425
JungSeongCheol <kir5121@naver.com>
2021-06-26 23:50:30 +0900

    C# 2445 문제 파일 생성

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example2445/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example2445
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
