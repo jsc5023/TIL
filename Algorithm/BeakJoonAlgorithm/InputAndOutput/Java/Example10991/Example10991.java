72b2be864fa2398295ad5812af46885b309253bc
JungSeongCheol <kir5121@naver.com>
2021-07-07 23:35:49 +0900

    Java 10991 문제풀이 완료

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example10991/Example10991.java
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/Java/Example10991/Example10991.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class Example10991 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			
+			for (int j = i; j < n - 1; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = 0; j < i * 2 + 1; j++) {
+				if(j % 2 == 0) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.print("\n");
+		}
+	}
 }
