9942c1ac192d7f72d3c69d38e3552719914fdaee
JungSeongCheol <kir5121@naver.com>
2021-07-08 23:47:08 +0900

    C# 10991 문제 풀이 완료

--- Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10991/Program.cs
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10991/Program.cs
@@ -10,7 +10,18 @@ namespace Example10991
 
             for (int i = 0; i < n; i++)
             {
-                Console.Write("*");
+                for (int j = i; j < n-1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < ( 2 * i ) + 1; j++)
+                {
+                    if (j % 2 == 0)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
 
                 Console.Write("\n");
             }
