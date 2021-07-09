61437ffbff75f90f7c8b9f9841cebf7213d3dc26
JungSeongCheol <kir5121@naver.com>
2021-07-09 23:46:36 +0900

    10992번 C# 문제풀이(미완성)

---
+++ Algorithm/BeakJoonAlgorithm/InputAndOutput/C#/InputAndOutput/Example10992/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example10992
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
