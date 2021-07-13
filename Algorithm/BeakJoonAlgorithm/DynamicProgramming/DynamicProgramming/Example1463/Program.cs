a957297eaf899a010040ae44d9808be76abca746
JungSeongCheol <kir5121@naver.com>
2021-07-13 23:39:12 +0900

    C# 1463 문제풀이(미완성)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/DynamicProgramming/Example1463/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/DynamicProgramming/Example1463/Program.cs
@@ -6,7 +6,31 @@ namespace Example1463
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 문제 풀이 방법 - 작은문제를 먼저 풀고, 그후 순차적으로 수행해 나간다.
+
+            int X = int.Parse(Console.ReadLine());
+            int[] dp = { 0, };
+
+            while(X != 1)
+            {
+                if (X % 3 == 0)
+                {
+                    X = X / 3;
+                    count += 1;
+                }  
+                else if (X % 2 == 0)
+                {
+                    X = X / 2;
+                    count += 1;
+                }
+                else
+                {
+                    X = X - 1;
+                    count += 1;
+                } 
+            }
+
+            Console.Write(count);
         }
     }
 }
