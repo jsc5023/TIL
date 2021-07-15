1f7f33629f0b9992df737f3c1c9d0f9b6c55b8bf
JungSeongCheol <kir5121@naver.com>
2021-07-16 07:27:32 +0900

    11726 Java 문제풀이(10007 나머지 구하기 추가)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example11726/Example11726.java
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example11726/Example11726.java
@@ -11,7 +11,7 @@ public class Example11726 {
 		dp[0] = dp[1] = 1;
 		
 		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i-1] + dp[i - 2];
+			dp[i] = (dp[i-1] + dp[i - 2]) % 10007;
 		}
 		
 		System.out.print(dp[n]);
