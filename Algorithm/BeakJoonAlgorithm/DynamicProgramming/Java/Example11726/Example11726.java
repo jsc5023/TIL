498c48e3c6f2973b7459d1ed9a4cdae78e7f8102
JungSeongCheol <kir5121@naver.com>
2021-07-16 07:24:54 +0900

    Java Example 11726 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example11726/Example11726.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Example11726 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int[] dp = new int[n+2];
+		
+		dp[0] = dp[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i-1] + dp[i - 2];
+		}
+		
+		System.out.print(dp[n]);
+	}
+}
