be54e3ae90fb4a026eab23055ffb98ebe7d45cfa
JungSeongCheol <kir5121@naver.com>
2021-07-17 11:34:13 +0900

    C

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11727/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example11727
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 점화식을 구하는것이 핵심입니다.
+
+            int n = int.Parse(Console.ReadLine());
+            int[] dp = new int[n + 2];
+
+            dp[0] = 0; dp[1] = 1; dp[2] = 3;
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = (dp[i - 1] + dp[i-2] * 2) % 10007;
+            }
+
+            Console.Write(dp[n]);
+        }
+    }
+}
