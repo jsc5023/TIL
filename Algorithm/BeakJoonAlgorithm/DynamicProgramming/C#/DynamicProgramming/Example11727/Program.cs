3554b5089197f93fa2fcfc6d356958973ef14ef9
JungSeongCheol <kir5121@naver.com>
2021-07-17 11:35:54 +0900

    11727 C# 풀이 (주석추가)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11727/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11727/Program.cs
@@ -7,6 +7,7 @@ namespace Example11727
         static void Main(string[] args)
         {
             // 점화식을 구하는것이 핵심입니다.
+            // 이런 타일링문제는, 보통 3~4개까지 값을 구해놓고, 그후 값을 찾는 방식을 찾는것이 기본입니다.
 
             int n = int.Parse(Console.ReadLine());
             int[] dp = new int[n + 2];
