4dce0d654518c7917a3c849a4c54e3c707123018
JungSeongCheol <kir5121@naver.com>
2021-07-18 23:04:40 +0900

    Example9095 문제 풀이 미완성

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9095/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Example9095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+
+            }
+        }
+    }
+}
