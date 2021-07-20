1eaa0c84c691f10c138624722aad780827d2d158
JungSeongCheol <kir5121@naver.com>
2021-07-20 23:46:11 +0900

    C# 9005 문제 풀이(미완성)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9095/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9095/Program.cs
@@ -6,6 +6,8 @@ namespace Example9095
     {
         static void Main(string[] args)
         {
+            //풀이법 1, 2, 3,4,5 를 직접 계산해서 점화식을 찾아야됩니다.
+            //그후 그 점화식을 바탕으로 수식을 작성합니다.
             int T = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < T; i++)
