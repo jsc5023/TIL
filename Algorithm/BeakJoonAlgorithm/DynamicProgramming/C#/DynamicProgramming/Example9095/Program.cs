99dff1c18fed919246dac3b6939808383f9fce2d
JungSeongCheol <kir5121@naver.com>
2021-07-21 23:11:38 +0900

    C# 9095 문제풀이 완료

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9095/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9095/Program.cs
@@ -14,7 +14,13 @@ namespace Example9095
             {
                 int n = int.Parse(Console.ReadLine());
 
-
+                int[] dp = new int[n + 3];
+                dp[1] = 1; dp[2] = 2; dp[3] = 4;
+                for (int j = 4; j <= n; j++)
+                {
+                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+                }
+                Console.WriteLine(dp[n]);
             }
         }
     }
