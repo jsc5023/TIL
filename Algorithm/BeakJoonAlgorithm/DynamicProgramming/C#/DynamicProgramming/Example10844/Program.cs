00364e012c9790ce7a16d7ebb1d932909341d474
JungSeongCheol <kir5121@naver.com>
2021-07-23 19:37:09 +0900

    Example10844 C# 추가(미완성)

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example10844/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example10844
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] dp = new int[N + 3];
+
+            dp[1] = 9; dp[2] = 17;
+
+
+
+        }
+    }
+}
