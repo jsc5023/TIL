9b96359b38968512d3a94f5c39d158327bae5efc
JungSeongCheol <kir5121@naver.com>
2021-07-25 23:02:02 +0900

    C# 10884 문제풀이 (인터넷 참조)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example10844/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example10844/Program.cs
@@ -6,16 +6,32 @@ namespace Example10844
     {
         static void Main(string[] args)
         {
+            int[,] dp = new int[101,10];
+            int sum = 0;
+
             int N = int.Parse(Console.ReadLine());
-            int[][] dp = new int[N + 3][];
+            for (int i = 0; i< 10; i++)
+	            dp[1,i] = 1;
 
-            //dp[1] = 9; dp[2] = 17;
-            for (int i = 0; i < n; i++)
+            for (int i = 2; i <= N; i++)
             {
-
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 0)
+                        dp[i,0] = dp[i - 1,1];
+                    else if (j == 9)
+                        dp[i,9] = dp[i - 1,8];
+                    else
+                        dp[i,j] = (dp[i - 1, j - 1] + dp[i - 1,j + 1]) % 1000000000;
+                }
             }
 
+            for (int i = 1; i < 10; i++)
+            {
+                sum = (sum + dp[N,i]) % 1000000000;
+            }
 
+            Console.Write(sum % 1000000000);
         }
     }
 }
