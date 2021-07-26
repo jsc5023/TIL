8f764debe66ad2550ccf545dc824c37c3b0055d8
JungSeongCheol <kir5121@naver.com>
2021-07-26 23:48:35 +0900

    10844 Java 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example10844/Example10844.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Example10844 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[][] dp = new int[100][101];
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+		
+	}
+}
