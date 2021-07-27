a095370d9573750250e680bd5c6f1dcb62aa554d
JungSeongCheol <kir5121@naver.com>
2021-07-27 23:46:10 +0900

    Java 10844 문제풀이 완료

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example10844/Example10844.java
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example10844/Example10844.java
@@ -5,11 +5,35 @@ public class Example10844 {
 		Scanner scan = new Scanner(System.in);
 		
 		int n = scan.nextInt();
-		int[][] dp = new int[100][101];
+		int[][] dp = new int[n+1][10];
 		
-		for (int i = 0; i < n; i++) {
-			
+		dp[1][0] = 0;
+		
+		for (int i = 1; i < 10; i++) {
+			dp[1][i] = 1;
 		}
+		
+		for (int i = 2; i <= n ; i++) {
+			dp[i][0] = (dp[i-1][0+1]) % 1000000000;
+			
+			dp[i][1] = (0 == dp[i-1][0]) ? 1 : dp[i-1][0]; // 0이면 1로계산, 아니면 그냥 그대로 들고온다
+			dp[i][1] += dp[i-1][2]; // 1이면 10, 12의값을 가지므로
+			dp[i][1] %= 1000000000;
+			
+			for (int j = 2; j <= 8; j++) {
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000;
+			}
+			
+			dp[i][9] = (dp[i-1][8] % 1000000000);
+		}
+		
+		int sum = 0;
+		for (int i = 1; i <= 9; i++) {
+			sum += dp[n][i];
+			sum %= 1000000000;
+		}
+		
+		System.out.print(sum);
 		
 	}
 }
