025938a4ec9acfc749a7838ad560223cb1414ff4
JungSeongCheol <kir5121@naver.com>
2021-07-29 23:09:50 +0900

    11057 C# 문제 풀이

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11057/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11057/Program.cs
@@ -6,7 +6,35 @@ namespace Example11057
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] dp = new int[1001, 10];
+
+            int ans = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                dp[1,i] = 1;
+            }
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int k = j; k < 10; k++)
+                    {
+                        dp[i,j] = (dp[i,j] + dp[i - 1, k]) % 10007;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                ans = (ans + dp[n, i]) % 10007;
+            }
+
+            Console.Write(ans);
         }
     }
 }
