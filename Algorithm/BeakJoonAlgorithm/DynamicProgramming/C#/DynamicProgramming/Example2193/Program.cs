42608b8c64541106412c35cdb9692182f7162fe7
JungSeongCheol <kir5121@naver.com>
2021-08-02 23:08:43 +0900

    2193 문제풀이(미완성)

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example2193/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example2193
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0으로 시작하지 않습니다.
+            // 1이 연속으로 나타나지않는다.
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] dp = new int[N+1];
+
+        }
+    }
+}
