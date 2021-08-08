83397101d3bc5fde20bebf232814d0425aa37a63
JungSeongCheol <kir5121@naver.com>
2021-08-08 21:23:06 +0900

    9465 새로풀이

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9465/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9465/Program.cs
@@ -6,20 +6,37 @@ namespace Example9465
     {
         static void Main(string[] args)
         {
-            int t = int.Parse(Console.ReadLine());
+            int[,] st = new int[2, 100001];
             int[,] dp = new int[2, 100001];
-            for (int i = 0; i < t; i++)
-            {
-                int n = int.Parse(Console.ReadLine());
 
-                for (int j = 0; j < 2; j++)
+            int t;
+            int n;
+
+            t = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= t; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < n; j++)
+                    st[0,j] = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < n; j++)
+                    st[1,j] = int.Parse(Console.ReadLine());
+
+                dp[0,0] = st[0,0];
+                dp[1,0] = st[1,0];
+                dp[0,1] = st[0,1] + dp[1,0];
+                dp[1,1] = st[1,1] + dp[0,0];
+
+                for (int j = 2; j < n; j++)
                 {
-                    for (int k = 1; k <= n; k++)
-                    {
-                        dp[j,k] = int.Parse(Console.ReadLine());
-                    }
+                    dp[0,j] = st[0,j] + Math.Max(dp[1,j - 1], dp[1,j - 2]);
+                    dp[1,j] = st[1,j] + Math.Max(dp[0,j - 1], dp[0,j - 2]);
                 }
-                Console.Write(dp[2, n]);
+
+                Console.WriteLine(Math.Max(dp[0,n - 1], dp[1,n - 1]));
+
             }
         }
     }
