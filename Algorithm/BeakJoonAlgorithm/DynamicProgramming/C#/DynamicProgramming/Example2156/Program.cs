b59de715688c563f135649567fb4074ab3ad4ca2
JungSeongCheol <kir5121@naver.com>
2021-08-12 12:51:02 +0900

    C# 2156 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example2156/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Example2156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dt = new int[n + 1];
+            int[] dp = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                int juices = int.Parse(Console.ReadLine());
+
+                dt[i] = juices;
+            }
+
+            if(n <= 2)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    dp[n] += dt[i];
+                }
+            }
+            else
+            {
+                //dp[0] = dt[0];
+                //dp[1] = dt[0] + dt[1];
+                //for (int i = 2; i < n; i++)
+                //{
+                //    dp[i] = dp[i-1]
+                //}
+            }
+
+            Console.Write(dp[n]);
+        }
+    }
+}
