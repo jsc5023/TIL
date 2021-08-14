d8244ba6b019e0eee15621215cc812ba7b7a5cdb
JungSeongCheol <kir5121@naver.com>
2021-08-14 23:41:27 +0900

    11053 문제풀이(미완성)

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11053/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example11053
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dt = new int[n+1];
+            for (int i = 0; i < n; i++)
+            {
+                dt[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
