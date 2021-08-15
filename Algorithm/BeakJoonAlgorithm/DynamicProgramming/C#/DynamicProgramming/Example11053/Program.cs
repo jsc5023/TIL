23277b1137c95a2b003bc12dc6ec3222fea934d6
JungSeongCheol <kir5121@naver.com>
2021-08-15 22:04:33 +0900

    C#11053 미완성

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11053/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11053/Program.cs
@@ -13,6 +13,12 @@ namespace Example11053
             {
                 dt[i] = int.Parse(Console.ReadLine());
             }
+
+            int[] dp = new int[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                
+            }
         }
     }
 }
