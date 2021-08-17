2e79cb956eeaf0b20279a1e20219e057f1426520
JungSeongCheol <kir5121@naver.com>
2021-08-17 12:56:53 +0900

    11053 문제풀이(오답인데, 이유를 찾아야됨, 예제는 맞았음)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11053/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11053/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Example11053
 {
@@ -8,19 +9,27 @@ namespace Example11053
         {
             int n = int.Parse(Console.ReadLine());
 
-            int[] dt = new int[n+1];
-            for (int i = 0; i < n; i++)
+            int[] dt = new int[n + 1];
+
+            string[] firstRowString = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < firstRowString.Length; i++)
             {
-                dt[i] = int.Parse(Console.ReadLine());
+                dt[i] = int.Parse(firstRowString[i]);
             }
 
-            int[] dp = new int[n + 1];
-            for (int i = 0; i <= n; i++)
+            ArrayList dp = new ArrayList();
+
+            dp.Add(dt[0]);
+            for (int i = 1; i <= n; i++)
             {
-                dp[i] = Math.Max(dp[i], dp[i] + 1);
+                if((int)dp[dp.Count-1] <= dt[i])
+                {
+                    dp.Add(dt[i]);
+                }
             }
 
-            Console.Write(dp[n]);
+            Console.Write(dp.Count);
         }
     }
 }
