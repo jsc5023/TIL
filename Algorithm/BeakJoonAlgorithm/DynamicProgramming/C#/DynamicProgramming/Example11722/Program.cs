a93b60c19aec27e2b836f5a822e54ca2bf4be7e6
JungSeongCheol <kir5121@naver.com>
2021-08-19 08:30:54 +0900

    C# 11722 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11722/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Example11722
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] firstNum = Console.ReadLine().Split(' ');
+
+            int[] dt = new int[n + 1]; 
+            for (int i = 0; i < firstNum.Length; i++)
+            {
+                dt[i] = int.Parse(firstNum[i]);
+            }
+
+            int[] dp = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (dt[i] < dt[j] && dp[j] + 1 > dp[i])
+                        dp[i] = dp[j] + 1;
+                }
+            }
+
+            int arrlength = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arrlength = Math.Max(dp[i], arrlength);
+            }
+
+            Console.Write(arrlength);
+        }
+    }
+}
