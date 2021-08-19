5e3e90fd484c39649110c5dfe27bf8ffa9e86107
JungSeongCheol <kir5121@naver.com>
2021-08-19 12:45:46 +0900

    Java 11722 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/Java/Example11722/Example11722.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Example11722 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] dt = new int[n+1];
+		for (int i = 0; i < n; i++) {
+			dt[i] = sc.nextInt();
+		}
+		
+		int[] dp = new int[n+1];
+		
+		for (int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if(dt[j] > dt[i] && dp[j] + 1 > dp[i]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		
+		int arrLength = 0;
+		for (int i = 0; i < n; i++) {
+			arrLength = Math.max(dp[i], arrLength); 
+		}
+		
+		System.out.print(arrLength);
+	}
+
+}
