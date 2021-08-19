afabba055bc5823eeeceb2226438fb322bd86b8e
JungSeongCheol <kir5121@naver.com>
2021-08-20 08:40:13 +0900

    C# 11054 문제풀이(미완성)

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11054/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example11054
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] inputNum = Console.ReadLine().Split(' ');
+
+            int[] dt = new int[n + 1];
+            for (int i = 0; i < inputNum.Length; i++)
+                dt[i] = int.Parse(inputNum[i]);
+
+            int[] dpIncrease = new int[n + 1]; // 왼쪽에서 시작, 점점 증가하는 dp
+
+            for (int i = 0; i < n; i++){
+                dpIncrease[0] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (dt[j] < dt[i])
+                        dpIncrease[i] = dpIncrease[j] + 1;
+                }
+            }
+        }
+    }
+}
