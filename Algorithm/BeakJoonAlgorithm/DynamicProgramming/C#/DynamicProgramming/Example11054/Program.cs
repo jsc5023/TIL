69062f3bb708f46e6186aeaaffa4f1eb8d0ee76a
JungSeongCheol <kir5121@naver.com>
2021-08-20 12:53:37 +0900

    C# 11054 풀이중 (오답이 나왔는데 그이유를 모르겠음)

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11054/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11054/Program.cs
@@ -16,14 +16,36 @@ namespace Example11054
 
             int[] dpIncrease = new int[n + 1]; // 왼쪽에서 시작, 점점 증가하는 dp
 
-            for (int i = 0; i < n; i++){
-                dpIncrease[0] = 1;
+            dpIncrease[0] = 1;
+
+            for (int i = 1; i < n; i++){
+                dpIncrease[i] = 1;
                 for (int j = 0; j < i; j++)
                 {
-                    if (dt[j] < dt[i])
+                    if (dt[j] < dt[i] && dpIncrease[j] + 1 > dpIncrease[i])
                         dpIncrease[i] = dpIncrease[j] + 1;
                 }
             }
+
+            int[] dpDecrease = new int[n + 1];
+            dpIncrease[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                dpDecrease[i] = 1;
+                for (int j = n-1; j > i; j--)
+                {
+                    if (dt[j] < dt[i] && dpDecrease[j] + 1 > dpDecrease[i])
+                        dpDecrease[i] = dpDecrease[j] + 1;
+                }
+            }
+
+            int maxLength = 0;
+            for (int i = 0; i < n; i++)
+            {
+                maxLength = Math.Max(dpIncrease[i] + dpDecrease[i], maxLength);
+            }
+
+            Console.Write(maxLength);
         }
     }
 }
