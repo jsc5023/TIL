2e84daa603de3ad5eae7bf8b872534fc1fe06803
JungSeongCheol <kir5121@naver.com>
2021-08-21 23:39:41 +0900

    1912 문제풀이C#

--- Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11054/Program.cs
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example11054/Program.cs
@@ -15,19 +15,20 @@ namespace Example11054
                 dt[i] = int.Parse(inputNum[i]);
 
             int[] dpIncrease = new int[n + 1]; // 왼쪽에서 시작, 점점 증가하는 dp
+            int[] dpDecrease = new int[n + 1];
 
             dpIncrease[0] = 1;
-
             for (int i = 1; i < n; i++){
                 dpIncrease[i] = 1;
                 for (int j = 0; j < i; j++)
                 {
                     if (dt[j] < dt[i] && dpIncrease[j] + 1 > dpIncrease[i])
+                    {
                         dpIncrease[i] = dpIncrease[j] + 1;
+                    }  
                 }
             }
 
-            int[] dpDecrease = new int[n + 1];
             dpIncrease[n - 1] = 1;
             for (int i = n - 2; i >= 0; i--)
             {
@@ -35,7 +36,9 @@ namespace Example11054
                 for (int j = n-1; j > i; j--)
                 {
                     if (dt[j] < dt[i] && dpDecrease[j] + 1 > dpDecrease[i])
+                    {
                         dpDecrease[i] = dpDecrease[j] + 1;
+                    }
                 }
             }
 
---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example1912/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Example1912
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dt = new int[n + 1];
+
+            string[] inputString = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                dt[i] = int.Parse(inputString[i]);
+            }
+
+            int[] dp2 = new int[n + 1];
+            dp2[1] = Math.Max(dt[1], dt[0] + dt[1]);
+
+            for (int i = 2; i < n; i++)
+            {
+                dp2[i] = Math.Max(dp2[i - 1] + dt[i], dt[i]);
+            }
+
+            int[] dp = new int[n + 1];
+            dp[0] = dt[0];
+            for (int i = 1; i < n; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], dp2[i]);
+            }
+            dp[n - 1] = Math.Max(dp[0], dp[n - 1]);
+
+            Console.Write(dp[n - 1]);
+
+        }
+    }
+}
