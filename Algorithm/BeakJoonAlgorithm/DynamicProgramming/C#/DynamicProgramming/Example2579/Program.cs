833edbc20199e52165588a95ca1d45f53ec981ea
JungSeongCheol <kir5121@naver.com>
2021-08-22 23:02:49 +0900

    C# 2579 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example2579/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Example2579
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dt = new int[n + 2];
+            int[,] dp = new int[n + 2,3];
+            for (int i = 1; i <= n; i++)
+                dt[i] = int.Parse(Console.ReadLine());
+
+            dp[1,1] = dt[1];
+            dp[1,2] = 0;
+
+            dp[2,1] = dt[2];
+            dp[2,2] = dt[1] + dt[2];
+
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i,1] = Math.Max(dp[i - 2,1], dp[i - 2,2]) + dt[i];
+                dp[i,2] = dp[i - 1,1] + dt[i];
+            }
+
+            Console.Write(Math.Max(dp[n, 1], dp[n, 2]));
+        }
+    }
+}
