877811873a0a0d2443cf4cf5472bb095013e871c
JungSeongCheol <kir5121@naver.com>
2021-08-23 22:49:41 +0900

    1699 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example1699/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example1699
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] dp = new int[n + 2];
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = i; // 처음 최댓값 = 초기값, 1+1+1+... 은 무조건 성립하므로
+                for (int j = 1; j*j <= i; j++)
+                {
+                    if (dp[i] > dp[i - (j * j)] + 1)
+                        dp[i] = dp[i - (j * j)] + 1;
+                }
+            }
+
+            Console.Write(dp[n]);
+        }
+    }
+}
