e065f7939541726062c41e1b7eb11259e4b0bc7d
JungSeongCheol <kir5121@naver.com>
2021-08-25 12:55:23 +0900

    9461 문제풀이 완료

---
+++ Algorithm/BeakJoonAlgorithm/DynamicProgramming/C#/DynamicProgramming/Example9461/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Example9461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                ulong[] dp = new ulong[106];
+                dp[1] = 1;
+                dp[2] = 1;
+                dp[3] = 1;
+                dp[4] = 2;
+                dp[5] = 2;
+                for (int j = 6; j <= n; j++)
+                {
+                    dp[j] += dp[j - 1] + dp[j - 5];
+                }
+
+                Console.WriteLine(dp[n]); // Write하니까 틀렸음, line을 변경해야됨
+            }
+        }
+    }
+}
