22364b18b8b56827fe40a2118ed8be35a0844a57
JungSeongCheol <kir5121@naver.com>
2021-08-27 00:54:51 +0900

    C# 4153 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example4153/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Example4153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+
+                int[] n = new int[str.Length];
+
+                n[0] = int.Parse(str[0]);
+                n[1] = int.Parse(str[1]);
+                n[2] = int.Parse(str[2]);
+
+                if ((n[0] == 0) && (n[1] == 0) && (n[2] == 0))
+                    break;
+
+                int temp = 0;
+                if(n[0] > n[1])
+                {
+                    temp = n[0];
+                    n[0] = n[1];
+                    n[1] = temp;
+                }
+
+                if(n[1] > n[2])
+                {
+                    temp = n[1];
+                    n[1] = n[2];
+                    n[2] = temp;
+                }
+
+                if ((n[0] * n[0]) + (n[1] * n[1]) == (n[2] * n[2]))
+                    Console.WriteLine("right");
+                else
+                    Console.WriteLine("wrong");
+            }
+        }
+    }
+}
