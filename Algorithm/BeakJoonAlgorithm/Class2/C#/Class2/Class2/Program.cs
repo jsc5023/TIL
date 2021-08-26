f814a48d3cc6a45dfe84f60bce2aeb785de74579
JungSeongCheol <kir5121@naver.com>
2021-08-26 23:58:38 +0900

    백준 Class2 생성

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Class2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class2
+{
+    class Example1018
+    {
+        static void Main(string[] args)
+        {
+            string[] numstr = Console.ReadLine().Split(' ');
+
+            int N = int.Parse(numstr[0]);
+            int M = int.Parse(numstr[1]);
+
+            char[,] board = new char[N,M];
+            for (int i = 0; i < N; i++)
+            {
+                string str = Console.ReadLine();
+                for (int j = 0; j < M; j++)
+                {
+                    board[i, j] = str[j];
+                }
+            }
+
+            int k = 0;
+        }
+    }
+}
