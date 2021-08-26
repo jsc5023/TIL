a27bcd4da4efd73008ca7ce4d3d2ddfb896c833b
JungSeongCheol <kir5121@naver.com>
2021-08-27 00:24:17 +0900

    C# 1085 풀이

--- Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example11085/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example11085/Program.cs
@@ -6,7 +6,18 @@ namespace Example11085
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] str = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(str[0]);
+            int y = int.Parse(str[1]);
+            int w = int.Parse(str[2]);
+            int h = int.Parse(str[3]);
+
+            int min = Math.Min(x - 0, w - x);
+            min = Math.Min(min, y - 0);
+            min = Math.Min(min, h - y);
+
+            Console.Write(min);
         }
     }
 }
