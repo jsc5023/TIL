5e00746b3106810396445934b97c877f5898f25c
JungSeongCheol <kir5121@naver.com>
2021-08-28 00:46:01 +0900

    C# 2231 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example2231/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Example2231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            string digit = "";
+            int[] digitNum = new int[8];
+            for (int i = 0; i < n; i++)
+            {
+                digit = i.ToString();
+                for (int j = 0; j < digit.Length; j++)
+                {
+                    digitNum[j] = int.Parse(digit[j].ToString());
+                    result += digitNum[j];
+
+                    if(j == digit.Length - 1)
+                    {
+                        result += i;
+                        if(result == n)
+                        {
+                            Console.Write(i);
+                            return;
+                        }
+                    }
+                }
+                result = 0;
+            }
+
+            Console.Write(0);
+        }
+    }
+}
