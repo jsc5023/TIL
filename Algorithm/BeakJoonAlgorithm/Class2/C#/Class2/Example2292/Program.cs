4daadd6949e0ee98be86f436bdfe3b277dfec8d7
JungSeongCheol <kir5121@naver.com>
2021-08-28 23:13:41 +0900

    C# 2292 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example2292/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Example2292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            int sum = 1;
+            if (N == 1){
+                result = 1;
+                Console.Write(result);
+                return;
+            }
+                
+            int i = 0;
+
+            while (true)
+            {
+                i++;
+                sum += i * 6;
+                if (sum >= N)
+                {
+                    result = i + 1;
+                    break;
+                }
+            }
+
+            Console.Write(result);
+        }
+    }
+}
