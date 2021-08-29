570cd0c411cb36e290cac2aa1675779b9ca3d241
JungSeongCheol <kir5121@naver.com>
2021-08-29 11:49:40 +0900

    C# 2775 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example2775/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Example2775
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                int k = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+                int[,] dp = new int[15, 15];
+
+                for (int j = 0; j < 15; j++)
+                {
+                    dp[0, j] = j;
+                }
+                for (int j = 1; j <= k; j++)
+                {
+                    for (int l = 1; l <= n; l++)
+                    {
+                        for (int m = 0; m <= l; m++)
+                        {
+                            dp[j, l] += dp[j - 1, m];
+                        }
+                    }
+                }
+                Console.WriteLine(dp[k, n]);
+            }
+        }
+    }
+}
