13c72815b8af25d8fbadbf9a34ef0396e2f5e8cf
JungSeongCheol <kir5121@naver.com>
2021-08-30 23:27:13 +0900

    C# 2869 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example2869/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example2869/Program.cs
@@ -6,7 +6,19 @@ namespace Example2869
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] str = Console.ReadLine().Split(' ');
+
+            double A = double.Parse(str[0]);
+            double B = double.Parse(str[1]);
+            double V = double.Parse(str[2]);
+
+            double result = 0;
+            double diff = A - B;
+
+            result = Math.Ceiling((V - A) / diff) + 1;
+
+            Console.Write(result);
+            
         }
     }
 }
