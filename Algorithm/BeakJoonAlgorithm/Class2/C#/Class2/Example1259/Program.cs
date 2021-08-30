f99c856c5a783785c496f837546399b6efff209e
JungSeongCheol <kir5121@naver.com>
2021-08-30 12:53:01 +0900

    C# 1259 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1259/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example1259
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+
+                if (str == "0")
+                    return;
+                string reverse = "";
+
+                for (int i = str.Length - 1; i >= 0 ; i--)
+                {
+                    reverse += str[i];
+                }
+
+                if(str == reverse)
+                    Console.WriteLine("yes");
+                else
+                    Console.WriteLine("no");
+            }
+        }
+    }
+}
