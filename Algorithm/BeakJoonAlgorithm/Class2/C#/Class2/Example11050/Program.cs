6d9ebe5c5b272892ea12c3bf3a486856fe048687
JungSeongCheol <kir5121@naver.com>
2021-08-31 00:43:28 +0900

    C# 11050 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example11050/Program.cs
@@ -0,0 +1,32 @@
+﻿    using System;
+
+    namespace Example11050
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+
+                int N = int.Parse(str[0]);
+                int K = int.Parse(str[1]); // N컴비네이션 K
+
+                int top = 1;
+                int result = 0;
+                for (int i = N; i > N-K; i--)
+                {
+                    top *= i;
+                }
+
+                int bottom = 1;
+                for (int i = 1; i <= K; i++)
+                {
+                    bottom *= i;
+                }
+
+                result = top / bottom;
+
+                Console.Write(result);
+            }
+        }
+    }
