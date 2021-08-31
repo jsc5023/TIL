cb5a8bcca79ad3a93bcfd867cc23bc208be61544
JungSeongCheol <kir5121@naver.com>
2021-09-01 00:01:05 +0900

    C# 1018 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Class2/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Class2/Program.cs
@@ -4,6 +4,37 @@ namespace Class2
 {
     class Example1018
     {
+        const int W = 0;
+        const int B = 1;
+        // 배열은 C#에서 const를 사용할 수 없다. 런타임에만 배열을 만들 수 있다.
+        // 그런데 const는 런타임때는 해석이 되는 경우므로, 상수 초기화가 되지 않아서 오류가 나옵니다.
+        // 그래서 읽기 전용으로 선언하는 것이다.
+        static readonly int[,] boardW = { 
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W }
+        };
+
+        static readonly int[,] boardB = {
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B },
+            { B, W, B, W, B, W, B, W },
+            { W, B, W, B, W, B, W, B }
+        };
+
+        static int[,] board;
+       
+        static int result = 64; // 8 * 8(최대값)
+
         static void Main(string[] args)
         {
             string[] numstr = Console.ReadLine().Split(' ');
@@ -11,17 +42,49 @@ namespace Class2
             int N = int.Parse(numstr[0]);
             int M = int.Parse(numstr[1]);
 
-            char[,] board = new char[N,M];
+            board = new int[N, M];
+
             for (int i = 0; i < N; i++)
             {
                 string str = Console.ReadLine();
                 for (int j = 0; j < M; j++)
                 {
-                    board[i, j] = str[j];
+                    if(str[j] == 'W')
+                        board[i, j] = 0;
+                    else
+                        board[i, j] = 1;
                 }
             }
 
-            int k = 0;
+            for (int i = 0; i <= N - 8; i++)
+            {
+                for (int j = 0; j <= M - 8; j++)
+                {
+                    compare(i, j);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public static void compare(int a, int b)
+        {
+            int temp1 = 0, temp2 = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (board[a + i, b + j] != boardW[i, j])
+                    {
+                        temp1++;
+                    }
+                    if (board[a + i, b + j] != boardB[i, j]) temp2++;
+                }
+            }
+
+            result = Math.Min(result, temp1);
+            result = Math.Min(result, temp2);
         }
     }
 }
