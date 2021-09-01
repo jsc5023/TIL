cb598ed7b3b8a9a9602c65aa3d075d82ce7ae2d6
JungSeongCheol <kir5121@naver.com>
2021-09-01 22:45:40 +0900

    1181 시간초과(선택정렬 사용)

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1181/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Example1181
+{
+    class Program
+    {
+        static string[] str = new string[20001];
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+
+            int smallIndex;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallIndex = i;
+                for (int j = i+1; j < n; j++)
+                {
+                    if(str[j].Length < str[smallIndex].Length)
+                    {
+                        smallIndex = j;
+                    }
+                    else if(str[j].Length == str[smallIndex].Length)
+                    {
+                        for ( int k = 0; k < str[smallIndex].Length; k++)
+                        {
+                            if(str[j][k] < str[smallIndex][k])
+                            {
+                                smallIndex = j;
+                            }
+                        }
+                    }
+                }
+                smallChange(i, smallIndex);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+        }
+
+        static void smallChange(int first, int second)
+        {
+            string temp = "";
+            temp = str[first];
+            str[first] = str[second];
+            str[second] = temp;
+        }
+    }
+}
