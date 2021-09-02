bfa5a6d8552a308d7b24f7a072044d09ae8e3b95
JungSeongCheol <kir5121@naver.com>
2021-09-02 22:33:14 +0900

    C# 1436 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1436/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Example1436
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int cnt = 1;
+            int number = 666;
+            while(N > cnt)
+            {
+                number++;
+                if(number.ToString().Contains("666"))
+                    cnt++;
+            }
+            System.Console.Write(number);
+        }
+    }
+}
