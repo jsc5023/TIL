b9dbb05cf7333c50799553273537d1ca76d6f6b3
JungSeongCheol <kir5121@naver.com>
2021-09-06 23:15:11 +0900

    10989 답은 맞음, 메모리 주의

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example10989/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Example10989
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[10001];
+            for (int i = 0; i < N; i++)
+            {
+                arr[int.Parse(Console.ReadLine())]++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < 10001; i++)
+            {
+                if(i % 100 == 0)
+                {
+                    Console.Write(sb);
+                    sb = new StringBuilder();
+                }
+                while (arr[i] > 0)
+                {
+                    sb.Append(i).Append("\n");
+                    arr[i]--;
+                }
+            }
+        }
+    }
+}
