1b9eef27d957842a544b3f8de49c8cae6a4ef786
JungSeongCheol <kir5121@naver.com>
2021-09-07 22:44:30 +0900

    Java 11651 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1920/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example1920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
