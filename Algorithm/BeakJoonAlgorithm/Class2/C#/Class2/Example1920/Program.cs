0babc72071e3a454997929ccc8751fbf8b01dea1
JungSeongCheol <kir5121@naver.com>
2021-09-08 00:33:43 +0900

    C# 1920 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1920/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1920/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Text;
 
 namespace Example1920
 {
     class Program
     {
+        public static int binarySearch(int[] arr, int findNum)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if(findNum < arr[mid])
+                {
+                    right = mid - 1;
+                }
+                else if(findNum > arr[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] arrN = new int[N];
+
+            string[] str = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                arrN[i] = int.Parse(str[i]);
+            }
+
+            Array.Sort(arrN);
+
+
+            int M = int.Parse(Console.ReadLine());
+
+            str = Console.ReadLine().Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < M; i++)
+            {
+                int findNum = int.Parse(str[i]);
+                sb.Append(binarySearch(arrN, findNum)).Append("\n");
+            }
+
+            Console.Write(sb);
         }
     }
 }
