5c81d3a76a5255210c94a38e0fe405f115c348e5
JungSeongCheol <kir5121@naver.com>
2021-09-08 22:04:47 +0900

    C#1978 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1978/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Example1978
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] str = Console.ReadLine().Split(' ');
+
+            int[] num = new int[1001];
+            int[] inputnum = new int[n];
+
+            num[1] = 1;
+            for (int i = 1; i <= 1000; i++)
+            {
+                for (int j = 2; j <i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        num[i] = 1;
+                        break;
+                    }
+                }
+            }
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int nowNum = int.Parse(str[i]);
+                if(num[nowNum] == 0)
+                {
+                    cnt++;
+                }
+            }
+            Console.Write(cnt);
+        }
+    }
+}
