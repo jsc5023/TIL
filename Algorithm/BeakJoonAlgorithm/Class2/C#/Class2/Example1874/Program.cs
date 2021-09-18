e1dcb00ebab3c3dfdc4211328ecd1904f956d8bb
JungSeongCheol <kir5121@naver.com>
2021-09-18 19:33:53 +0900

    C# 1874 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class2/C#/Class2/Example1874/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example1874
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            StringBuilder sb = new StringBuilder();
+
+            int start = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int inputStack = int.Parse(Console.ReadLine());
+
+                if (inputStack > start)
+                {
+                    for (int j = start + 1; j <= inputStack; j++)
+                    {
+                        stack.Push(j);
+                        sb.Append("+").Append("\n");
+                    }
+                    start = inputStack;
+                }
+
+                else if (stack.Peek() != inputStack)
+                {
+                    Console.Write("NO");
+                    return;
+                }
+
+                stack.Pop();
+                sb.Append("-").Append("\n");
+
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
