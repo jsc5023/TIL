21cb6e32255c24d5b1b982a0a760104dca3626e7
JungSeongCheol <kir5121@naver.com>
2021-09-22 21:50:54 +0900

    C# 1676 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example1676/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Example1676
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int cnt2 = 0;
+            int cnt5 = 0;
+            int result = 0;
+            for (int i = N; i > 0; i--)
+            {
+                int temp = i;
+                while (temp > 0)
+                {
+                    if (temp % 2 == 0)
+                        cnt2++;
+                    else
+                        break;
+                    temp /= 2;
+                }
+
+                temp = i;
+
+                while (temp > 0)
+                {
+                    if (temp % 5 == 0)
+                        cnt5++;
+                    else
+                        break;
+                    temp /= 5;
+                }
+            }
+
+            result = Math.Min(cnt2, cnt5);
+            Console.Write(result);
+        }
+    }
+}
