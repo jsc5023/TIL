b7cb06c461324a4f99cc4ebdb017d210698bf269
JungSeongCheol <kir5121@naver.com>
2021-09-24 23:19:15 +0900

    17626 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example17626/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Example17626
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dp = new int[n + 1];
+
+            dp[0] = 0;
+            dp[1] = 1;
+
+            int min = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                min = int.MaxValue;
+                for (int j = 1; j * j <= i; j++)
+                {
+                    min = Math.Min(min, dp[i - (j * j)]);
+                }
+
+                dp[i] = min + 1;
+            }
+
+            Console.Write(dp[n]);
+        }
+    }
+}
