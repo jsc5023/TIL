f44572cdd22c277f7315d7981d86b1fb46576f90
JungSeongCheol <kir5121@naver.com>
2021-09-25 21:35:28 +0900

    1003 C# 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example1003/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Example1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            int[,] dp = new int[2, 41];
+
+            dp[0, 0] = 1;
+            dp[1, 0] = 0;
+
+            dp[0, 1] = 0;
+            dp[1, 1] = 1;
+
+            for (int i = 0; i <= 1; i++)
+            {
+                for (int j = 2; j <= 40; j++)
+                {
+                    dp[i, j] = dp[i, j - 1] + dp[i, j - 2];
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < T; i++)
+            {
+                int N = int.Parse(Console.ReadLine());
+
+                sb.Append(dp[0, N].ToString() + " " + dp[1, N].ToString()).Append("\n");
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
