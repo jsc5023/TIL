0cf0335c0f84b9cb54628657109e5e6ac743fa2e
JungSeongCheol <kir5121@naver.com>
2021-10-01 23:18:07 +0900

    C# 1451 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example1541/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Example1541
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string[] minusSplit = s.Split('-');
+
+            int[] plusResult = new int[minusSplit.Length];
+            for (int i = 0; i < minusSplit.Length; i++)
+            {
+                string[] plusNum = minusSplit[i].Split('+');
+
+                for (int j = 0; j < plusNum.Length; j++)
+                {
+                    plusResult[i] += int.Parse(plusNum[j]);
+                }
+            }
+
+            int sum = plusResult[0];
+
+            for (int i = 1; i < plusResult.Length; i++)
+            {
+                sum -= plusResult[i];
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
