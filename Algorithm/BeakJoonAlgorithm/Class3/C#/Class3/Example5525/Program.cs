918d330f3a3b2ed8c54ce652bf2e110b696f3ac0
JungSeongCheol <kir5121@naver.com>
2021-10-04 22:47:43 +0900

    C# 5525 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example5525/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Example5525
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int M = int.Parse(Console.ReadLine());
+            char[] s = Console.ReadLine().ToCharArray();
+
+            int cnt = 0;
+            int patternCnt = 0;
+            for (int i = 1; i < M - 1; i++)
+            {
+                if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I')
+                {
+                    patternCnt++;
+                    if (patternCnt == N)
+                    {
+                        patternCnt--;
+                        cnt++;
+                    }
+                    i++;
+                }
+                else
+                    patternCnt = 0;                    
+            }
+
+            Console.Write(cnt);
+        }
+    }
+}
