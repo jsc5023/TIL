7c46907088da8b5c20c43be902e2f5b994c3d7c1
JungSeongCheol <kir5121@naver.com>
2021-10-05 21:02:51 +0900

    11047 C# 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example11047/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Example11047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            int N = int.Parse(s[0]);
+            int K = int.Parse(s[1]);
+
+            int[] arr = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int cnt = 0;
+            int index = N - 1;
+
+            while (K > 0)
+            {
+                if(K / arr[index] > 0)
+                {
+                    cnt += (K / arr[index]);
+                    K %= arr[index];
+                }
+
+                index--;
+            }
+
+            Console.Write(cnt);
+        }
+    }
+}
