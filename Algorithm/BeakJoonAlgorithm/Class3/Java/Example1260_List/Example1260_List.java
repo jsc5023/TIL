674667ed1c5fdd32469f758ef0e144399822b208
JungSeongCheol <kir5121@naver.com>
2021-10-05 23:51:28 +0900

    Java 1260 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class3/Java/Example1260_List/Example1260_List.java
+++ Algorithm/BeakJoonAlgorithm/Class3/Java/Example1260_List/Example1260_List.java
@@ -1,42 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Example1260_Array {
-
-	static int N, M, V;
-	static int[][] arr;
+public class Example1260_List {
+	
+	static ArrayList<Integer>[] list;
 	static boolean[] visit;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1][N+1];
+		list = new ArrayList[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+
 		visit = new boolean[N+1];
-		
 		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
 			int v1 = Integer.parseInt(st.nextToken());
 			int v2 = Integer.parseInt(st.nextToken());
 			
-			arr[v1][v2] = 1;
-			arr[v2][v1] = 1;
+			list[v1].add(v2);
+			list[v2].add(v1);
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(list[i]);
 		}
 		
 		Arrays.fill(visit, false);
 		dfs(V);
 		System.out.println();
-		
 		Arrays.fill(visit, false);
 		bfs(V);
 	}
@@ -47,32 +56,30 @@ public class Example1260_Array {
 		
 		System.out.print(v + " ");
 		
-		for (int i = 1; i <= N; i++) {
-			if(arr[v][i] == 1 && visit[i] == false) {
-				dfs(i);
+		for (int item : list[v]) {
+			if(!visit[item]) {
+				dfs(item);
 			}
 		}
-		
 	}
 	
 	public static void bfs(int v) {
 		
-		Queue<Integer> queue = new LinkedList<Integer>();
+		Queue<Integer> queue = new LinkedList<>();
 		queue.offer(v);
 		visit[v] = true;
-
+		
+		
 		while(!queue.isEmpty()) {
 			int temp = queue.poll();
-			
 			System.out.print(temp + " ");
-			for (int i = 1; i <= N; i++) {
-				if(arr[temp][i] == 1 && visit[i] == false) {
-					queue.offer(i);
-					visit[i] = true;
+			for (Integer item : list[temp]) {
+				if(!visit[item]) {
+					queue.offer(item);
+					visit[item] = true;
 				}
 					
 			}
 		}
 	}
-
 }
