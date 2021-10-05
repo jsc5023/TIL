e8190694df63661cd365c65804024d8894ab8f3d
JungSeongCheol <kir5121@naver.com>
2021-10-06 00:10:15 +0900

    1541 Java 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class3/Java/Example1541/Example1541.java
+++ Algorithm/BeakJoonAlgorithm/Class3/Java/Example1541/Example1541.java
@@ -7,11 +7,27 @@ public class Example1541 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String[] expression = br.readLine().split("-");
+		String str = br.readLine();
 		
+		String[] minusStr = str.split("-");
 		
+		int sum = 0;
 		
+		for (int i = 0; i < minusStr.length; i++) {
+			int plusSum = 0;
+			String[] plusStr = minusStr[i].split("\\+");
+			
+			for (int j = 0; j < plusStr.length; j++) {
+				plusSum += Integer.parseInt(plusStr[j]);
+			}
+			
+			if(i == 0)
+				sum += plusSum;
+			else
+				sum -= plusSum;
+		}
 		
+		System.out.print(sum);
 	}
 
 }
