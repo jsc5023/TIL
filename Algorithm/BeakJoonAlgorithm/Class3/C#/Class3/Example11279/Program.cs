aa9dc89ca66aa3f22bf7ba227bb5dbbe6b559cd4
JungSeongCheol <kir5121@naver.com>
2021-10-06 23:00:11 +0900

    11279 C#풀이(아직 덜함, .Net6에서 부터 우선순위 큐가 나타나서 지금ㅎ면재로서는 새로구현해야됨)

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example11279/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example11279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()); // C#은 우선순위 큐가 없음 -> 구현해야됨
+            //LinkedList<int> ls = new LinkedList<int>();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == 0)
+                {
+                    if (list.Count == 0)
+                        sb.Append(0).Append("\n");
+                    else
+                    {
+                        int[] a = (int[])list.ToArray(typeof(int));
+                        Array.Sort(a);
+                        sb.Append(a[a.Length - 1]).Append("\n");
+                        list.Remove(a[a.Length - 1]);
+                    }
+                }
+                else
+                    list.Add(x);
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
