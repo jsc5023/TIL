7d4607b0935fdd68ba5c426681541c81b6e2b147
JungSeongCheol <kir5121@naver.com>
2021-10-07 01:07:09 +0900

    C# 1931 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example1931/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example1931/Program.cs
@@ -9,29 +9,23 @@ namespace Example1931
         {
             int N = int.Parse(Console.ReadLine());
 
-            int[][] arr = new int[N][];
-
+            List<List<int>> list = new List<List<int>>();
             for (int i = 0; i < N; i++)
             {
                 string[] s = Console.ReadLine().Split(' ');
 
-                arr[i][0] = int.Parse(s[0]);
-                arr[i][1] = int.Parse(s[1]);
+                list.Add(new List<int> { int.Parse(s[0]), int.Parse(s[1]) });
             }
 
-            //IOrderedEnumerable<int[]> sortedByFirst = arr.OrderBy(y => y[0]);
-            IOrderedEnumerable<int[]> sortedBySecond = arr.OrderBy(y => y[1]);
-
-            int[][] sortedArray = sortedBySecond.ToArray();
-
+            var orderList = list.OrderBy(x => x[1]).ThenBy(x => x[0]).ToList();
             int count = 0;
-            int prev_end_time = 0;
+            int prev = 0;
 
             for (int i = 0; i < N; i++)
             {
-                if (prev_end_time <= arr[i][0])
+                if (prev <= orderList[i][0])
                 {
-                    prev_end_time = arr[i][1];
+                    prev = orderList[i][1];
                     count++;
                 }
             }
