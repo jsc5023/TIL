4a47ccd4b0903babc3f16311de25eb23bd80279a
JungSeongCheol <kir5121@naver.com>
2021-10-07 00:21:06 +0900

    11727 문제풀이

--- Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example11727/Program.cs
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example11727/Program.cs
@@ -7,13 +7,14 @@ namespace Example11727
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
-            int[] dp = new int[N + 1];
+            int[] dp = new int[N + 2];
 
             dp[1] = 1;
             dp[2] = 3;
             for (int i = 3; i <= N; i++)
             {
-                dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+                dp[i] = (dp[i - 1] % 10007) + (2 * dp[i - 2] % 10007) ;
+                dp[i] %= 10007;
             }
 
             Console.Write(dp[N]);
