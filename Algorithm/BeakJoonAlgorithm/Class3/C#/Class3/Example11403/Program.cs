ce0697fe37976d009c59c1eb4126a8b4a1b2c460
JungSeongCheol <kir5121@naver.com>
2021-10-20 08:36:05 +0900

    11403 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class3/C#/Class3/Example11403/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Example11403
+{
+    class Program
+    {
+        static int[,] map;
+        static int[,] resultMap;
+        static bool[,] visit;
+        static int N;
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+
+            map = new int[N, N];
+            resultMap = new int[N, N];
+            visit = new bool[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < N; j++)
+                {
+                    map[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            for (int k = 0; k < N; k++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (map[i, k] == 1 && map[k, j] == 1)
+                        {
+                            map[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    sb.Append(map[i, j] + " ");
+                }
+                sb.Append("\n");
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
