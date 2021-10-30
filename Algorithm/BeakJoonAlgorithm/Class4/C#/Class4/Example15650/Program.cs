90c02a95ae216222d11cd74de2b4ea051ed20409
JungSeongCheol <kir5121@naver.com>
2021-10-30 22:36:51 +0900

    15650 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class4/C#/Class4/Example15650/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Example15650
+{
+    class Program
+    {
+        // 백트래킹 문제
+        public static int[] arr;
+        public static int N, M;
+
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            N = int.Parse(s[0]);
+            M = int.Parse(s[1]);
+
+            arr = new int[M];
+
+            dfs(1, 0);
+        }
+
+        static void dfs(int at, int depth)
+        {
+
+            if (depth == M)
+            {
+                foreach (var item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.Write("\n");
+                return;
+            }
+
+            for (int i = at; i <= N; i++)
+            {
+                arr[depth] = i;
+                dfs(i + 1, depth + 1);
+            }
+        }
+    }
+}
