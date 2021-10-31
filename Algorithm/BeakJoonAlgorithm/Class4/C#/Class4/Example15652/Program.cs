56ccf37c0d5a0624d489e3f456d3ad2d457ddfe1
JungSeongCheol <kir5121@naver.com>
2021-10-31 23:50:29 +0900

    15652 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class4/C#/Class4/Example15652/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Example15652
+{
+    class Program
+    {
+        // 15650이랑 거의 같은문제
+        static int N, M;
+        static int[] arr;
+
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            N = int.Parse(s[0]);
+            M = int.Parse(s[1]);
+            arr = new int[M];
+
+            dfs(1, 0);
+        }
+
+        public static void dfs(int at, int depth)
+        {
+            if (depth == M)
+            {
+                foreach (var item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.Write("\n");
+                return;
+            }
+
+            for (int i = at; i <= N; i++)
+            {
+                arr[depth] = i;
+                dfs(i, depth + 1);
+            }
+
+        }
+    }
+}
