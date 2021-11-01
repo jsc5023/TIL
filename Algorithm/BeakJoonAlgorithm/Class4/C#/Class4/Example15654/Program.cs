76a3d1a673a0fdbe19d2b9a9fe9bfce1a0a6d7c2
JungSeongCheol <kir5121@naver.com>
2021-11-01 23:07:35 +0900

    15654 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class4/C#/Class4/Example15654/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace Example15654
+{
+    class Program
+    {
+        static int N, M;
+        static int[] arr;
+        static int[] result;
+        static bool[] visited;
+        static StringBuilder sb;
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            N = int.Parse(s[0]);
+            M = int.Parse(s[1]);
+
+            arr = new int[N];
+            result = new int[M];
+            visited = new bool[N + 1];
+
+            s = Console.ReadLine().Split(' ');
+
+            sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = int.Parse(s[i]);
+            }
+
+            Array.Sort(arr);
+
+            cycle(0);
+
+            Console.Write(sb);
+        }
+
+        static void cycle(int start)
+        {
+            
+            if (start == M)
+            {
+                for (int i = 0; i < result.Length; i++)
+                {
+                    sb.Append(result[i] + " ");
+                }
+                sb.Append("\n");
+            }
+            else
+            {
+
+                for (int i = 0; i < N; i++)
+                {
+                    if (!visited[i])
+                    {
+                        visited[i] = true;
+                        result[start] = arr[i];
+                        cycle(start + 1);
+                        visited[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
