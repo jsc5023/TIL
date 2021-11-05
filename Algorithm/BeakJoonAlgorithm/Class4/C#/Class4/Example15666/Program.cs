90c061ff67cf3cef63c8482126be5c1dda45854c
JungSeongCheol <kir5121@naver.com>
2021-11-05 23:15:59 +0900

    15666 문제풀이

---
+++ Algorithm/BeakJoonAlgorithm/Class4/C#/Class4/Example15666/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace Example15666
+{
+    class Program
+    {
+        static int N, M;
+        static int[] arr;
+        static int[] ans;
+        static StringBuilder sb = new StringBuilder(); 
+
+
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            N = int.Parse(s[0]);
+            M = int.Parse(s[1]);
+
+            s = Console.ReadLine().Split(' ');
+
+            arr = new int[N];
+            ans = new int[M];
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = int.Parse(s[i]);
+            }
+
+            Array.Sort(arr);
+            dfs(0, 0);
+
+            Console.Write(sb);
+        }
+
+        private static void dfs(int start, int cnt)
+        {
+
+            if (cnt == M)
+            {
+                for (int i = 0; i < M; i++)
+                {
+                    sb.Append(ans[i] + " ");
+                }
+                sb.Append("\n");
+
+            }
+            else
+            {
+
+                int num = 0;
+                for (int i = start; i < N; i++)
+                {
+                    if (num == arr[i]) continue;
+
+                    ans[cnt] = arr[i];
+                    dfs(i, cnt + 1);
+                    num = arr[i];
+                }
+            }
+        }
+    }
+}
