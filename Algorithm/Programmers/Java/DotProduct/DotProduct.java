f8bbc99ac9495ac7812ec2163e065d7c0ee57ee3
JungSeongCheol <kir5121@naver.com>
2021-11-18 23:11:38 +0900

    내적 문제풀이

---
+++ Algorithm/Programmers/Java/DotProduct/DotProduct.java
@@ -0,0 +1,19 @@
+
+public class DotProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,2,3,4};
+		int[] b = {-3,-1,0,2};
+		System.out.print(solution(a, b));
+	}
+
+	public static int solution(int[] a, int[] b) {
+        
+		int answer = 0;
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+		return answer;
+    }
+}
