9e04bf11e2be57af9e2ba437ecb9f33caa513ed3
JungSeongCheol <kir5121@naver.com>
2021-11-20 01:52:06 +0900

    키패드 누르기 재풀이

--- Algorithm/Programmers/Java/PressKeyPad/PressKeyPad.java
+++ Algorithm/Programmers/Java/PressKeyPad/PressKeyPad.java
@@ -1,114 +1,63 @@
-
 public class PressKeyPad {
-
-	static int[] leftPosition = {3, 0};
-	static int[] rightPosition = {3, 2};
-	static int[][] keyPad = {{1,2,3}, {4,5,6}, {7,8,9}, {10, 0, 11}};
+	
+	// https://zzang9ha.tistory.com/292 참조(계속 시도했는데 틀려서, 이해 및 재작성으로 사용함)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
-		String hand = "right";
+		String hand = "left";
 		
 		System.out.print(solution(numbers, hand));
 	}
 
 	public static String solution(int[] numbers, String hand) {
-        String answer = "";
-        
-        for (int i = 0; i < numbers.length; i++) {
-        	answer += findHand(numbers[i], hand);
-		}
-        
-        return answer;
-    }
-	
-	public static String findHand(int number, String hand) {
+		StringBuilder sb = new StringBuilder();
+		int leftIndex = 10;	 // '*' => 10으로 치환
+		int rightIndex = 12; // '#' => 12로 치환
 		
-		String reulstHand = "";
-		
-		switch (number) {
-			// 왼쪽
-			case 1:
-				reulstHand = "L";
-				leftPosition[0] = 0;
-				leftPosition[1] = 0;
-				break;
-			case 4:
-				reulstHand = "L";
-				leftPosition[0] = 1;
-				leftPosition[1] = 0;
-				break;
-			case 7:
-				reulstHand = "L";
-				leftPosition[0] = 2;
-				leftPosition[1] = 0;
-				break;
-				
-			case 2:
-				reulstHand = middlePositionNumber(0, 1, hand);
-				break;
-			case 5:
-				reulstHand = middlePositionNumber(1, 1, hand);
-				break;
-			case 8:
-				reulstHand = middlePositionNumber(2, 1, hand);
-				break;
-			case 0:
-				reulstHand = middlePositionNumber(3, 1, hand);
-				break;
-				
-			// 오른쪽
-			case 3:
-				reulstHand = "R";
-				rightPosition[0] = 0;
-				rightPosition[1] = 2;
-				break;
-			case 6:
-				reulstHand = "R";
-				rightPosition[0] = 1;
-				rightPosition[1] = 2;
-				break;
-			case 9:
-				reulstHand = "R";
-				rightPosition[0] = 2;
-				rightPosition[1] = 2;
-				break;
-		}
-		return reulstHand;
-	}
-	
-	public static String middlePositionNumber(int xPosition, int yPosition, String hand) {
-		
-		String reulstHand = "";
-		
-		int leftPositionLength = 0;
-		int rightPositionLength = 0;
-		
-		leftPositionLength = Math.abs(leftPosition[0] - xPosition) + Math.abs(leftPosition[1] - yPosition);
-		rightPositionLength = Math.abs(Math.abs(rightPosition[0] - xPosition) + Math.abs(rightPosition[1] - yPosition));
-		if(rightPositionLength > leftPositionLength) {
-			reulstHand = "L";
-			leftPosition[0] = xPosition;
-			leftPosition[1] = yPosition;
-		}
-		else if(rightPositionLength == leftPositionLength) {
-			if(hand == "right") {
-				reulstHand = "R";
-				rightPosition[0] = xPosition;
-				rightPosition[1] = yPosition;
+		for (int number : numbers) {
+			if(number == 1 || number == 4 || number == 7) {
+				sb.append("L");
+				leftIndex = number;
+			}
+			else if(number == 3 || number == 6 || number == 9) {
+				sb.append("R");
+				rightIndex = number;
 			}
 			else {
-				reulstHand = "L";
-				leftPosition[0] = xPosition;
-				leftPosition[1] = yPosition;
+				int leftLen = getLength(leftIndex, number);
+				int rightLen = getLength(rightIndex, number);
+				
+				if(leftLen > rightLen) {
+					sb.append("R");
+					rightIndex = number;
+				}
+				else if(leftLen == rightLen) {
+					if(hand.equals("left")) {
+						sb.append("L");
+						leftIndex = number;
+					} else {
+						sb.append("R");
+						rightIndex = number;
+					}
+				}
+				else {
+					sb.append("L");
+					leftIndex = number;
+				}
 			}
 		}
-		else {
-			reulstHand = "R";
-			rightPosition[0] = xPosition;
-			rightPosition[1] = yPosition;
-		}
+		return sb.toString();
+	}
+
+	public static int getLength(int index, int number) {
+		index = (index == 0) ? 11 : index;
+		number = (number == 0) ? 11 : number;
 		
-		return reulstHand;
+		int x = (index - 1) / 3;
+		int y = (index - 1) % 3;
+		int numberX = number / 3;
+		int numberY = 1;
+		
+		return Math.abs(x-numberX) + Math.abs(y-numberY);
 	}
 }
