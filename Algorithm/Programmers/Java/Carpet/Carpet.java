002080027bb7458d5d8ddd89157f9df4d66aa122
JungSeongCheol <kir5121@naver.com>
2021-12-04 23:41:59 +0900

    카펫 복습용 재풀이

--- Algorithm/Programmers/Java/Carpet/Carpet.java
+++ Algorithm/Programmers/Java/Carpet/Carpet.java
@@ -11,25 +11,25 @@ public class Carpet {
 
 	public static int[] solution(int brown, int yellow) {
         
+		//
 		int[] answer = new int[2];
-		 
-        int area = brown + yellow;
+		
+		int area = brown + yellow;
  
-        for (int i = 1; i <= area; i++) {
-            int row = i;            
-            int col = area / row; 
-            
-            if (row > col)
-                continue;
- 
-            if ((row - 2) * (col - 2) == yellow) {
-                answer[0] = col;
-                answer[1] = row;
-                return answer;
-            }
- 
-        }
- 
+		for (int i = 1; i <= area; i++) {
+			int row = i;
+			int col = area / row;
+			
+			if(row > col)
+				continue;
+			
+			if((row - 2) * (col - 2) == yellow) {
+				answer[0] = col;
+				answer[1] = row;
+				return answer;
+			}
+		}
+		
         return answer;
     }
 }
