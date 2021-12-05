1e0ca0979d87a3204967fe8c496b4cff4459aadb
JungSeongCheol <kir5121@naver.com>
2021-12-05 23:52:05 +0900

    가장 큰 숫자 만들기(미완성)

---
+++ Algorithm/Programmers/Java/MakeBigNumber/MakeBigNumber.java
@@ -0,0 +1,24 @@
+
+public class MakeBigNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number = "1924";
+		int k = 2;
+		System.out.print(solution(number, k));
+		
+	}
+
+	public static String solution(String number, int k) {
+        String answer = "";
+        
+        // 풀이방법 - 우선, 재귀형식으로 만들어서 일정 숫자 이내에서 만들수 있는 것을 만든다.
+        // 그후 Array.Sort로 최대값을 구한다.
+        
+        for (int i = 0; i < number.length(); i++) {
+			
+		}
+        return answer;
+    }
+	
+}
