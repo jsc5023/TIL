0a8dab15e7ce549879bc40aed29dcac2d2a956b8
JungSeongCheol <kir5121@naver.com>
2021-12-08 08:57:13 +0900

    올바른 괄호 문제풀이

---
+++ Algorithm/Programmers/Java/CorrectParenthesis/CorrectParenthesis.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class CorrectParenthesis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = ")()(";
+		System.out.print(solution(s));
+	}
+	
+	static boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        char[] getchar = s.toCharArray();
+        for (int i = 0; i < getchar.length; i++) {
+        	if(getchar[i] == '(')
+        		stack.add(getchar[i]);
+        	
+        	else if(getchar[i] == ')') {
+        		if(stack.isEmpty())
+        			return false;
+        		else
+        			stack.pop();
+        	}
+		}
+        
+        if(stack.isEmpty())
+        	return true;
+        else
+        	return false;
+    }
+
+}
