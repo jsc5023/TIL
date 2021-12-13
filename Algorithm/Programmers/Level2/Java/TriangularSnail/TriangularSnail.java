9fd8cd9efb5f6cf23bab38ab46f86e60daafcf1e
JungSeongCheol <kir5121@naver.com>
2021-12-13 23:40:20 +0900

    삼각 달팽이 문제풀이(미완성)

---
+++ Algorithm/Programmers/Level2/Java/TriangularSnail/TriangularSnail.java
@@ -0,0 +1,27 @@
+
+public class TriangularSnail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		System.out.print(solution(n));
+	}
+
+	public static int[] solution(int n) {
+        int[] answer = {};
+        
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+        	sum += n;
+		}
+        
+        int[] dp = new int[sum];
+        
+        // ÂüÁ¶
+        for (int i = 0; i < sum; i++) {
+			
+		}
+        
+        return answer;
+    }
+}
