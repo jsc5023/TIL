fbd5c8115c51ef5962b9f13254dbf3b11cc3946c
JungSeongCheol <kir5121@naver.com>
2021-12-18 23:35:52 +0900

    쿼드압축 후 갯수 세기(미완성, 생각이 잘안남, 나중에 다시풀기)

---
+++ Algorithm/Programmers/Level2/Java/QuadCompressionCount/QuadCompressionCount.java
@@ -0,0 +1,23 @@
+
+public class QuadCompressionCount {
+
+	static int[] dx = {-1, 0, 0, 1};
+	static int[] dy = {0, -1, 1, 0};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] arr = {{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}};
+		solution(arr);
+	}
+	
+	public static int[] solution(int[][] arr) {
+        int[] answer = {};
+        // 풀이방법
+        // 상하좌우 4단계 모두를 비교해서 풀어나가자.
+        
+        
+        return answer;
+    }
+
+}
