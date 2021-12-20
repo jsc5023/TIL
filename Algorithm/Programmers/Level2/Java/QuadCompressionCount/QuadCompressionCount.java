c6a9ac9d6d8d207ba1ddff890632938f14d4900c
JungSeongCheol <kir5121@naver.com>
2021-12-20 23:54:09 +0900

    쿼드 압축후 개수세기 문제풀이

--- Algorithm/Programmers/Level2/Java/QuadCompressionCount/QuadCompressionCount.java
+++ Algorithm/Programmers/Level2/Java/QuadCompressionCount/QuadCompressionCount.java
@@ -13,10 +13,29 @@ public class QuadCompressionCount {
 	
 	public void quad(int n, int y, int x, int[][] arr) {
 		
+		if(n == 1) {
+			answer[arr[y][x]]++;
+			return;
+		}
 		
+		if(isBlock(n, y, x, arr)) return;
+		
+		quad(n/2, y, x, arr);
+		quad(n/2, y + n/2, x, arr);
+		quad(n/2, y, x + n/2, arr);
+		quad(n/2, y + n/2, x + n/2, arr);
 	}
 	
 	public boolean isBlock(int n, int y, int x, int[][] arr) {
+		
+		for (int i = y; i < y + n; i++) {
+			for (int j = x; j < x + n; j++) {
+				if(arr[y][x] != arr[i][j]) return false;
+			}
+		}
+		
+		answer[arr[y][x]]++;
+		return true;
 		
 	}
 	
