0e5f559cd93c09d9743750c285fe9d170aa55100
JungSeongCheol <kir5121@naver.com>
2021-12-24 22:59:13 +0900

    전력망 문제풀이 완료(answer -1 -> answer 100으로도 변경, 재풀이완료)

--- Algorithm/Programmers/Level2/Java/PowerGrid/PowerGrid.java
+++ Algorithm/Programmers/Level2/Java/PowerGrid/PowerGrid.java
@@ -13,56 +13,56 @@ public class PowerGrid {
 	}
 
 	public static int solution(int n, int[][] wires) {
-        int answer = -1;
+        int answer = 101;
         // 이문제는 한번씩 다시풀어보는데 햇갈림, 3일동안 풀어봐야됨, 내일 다시 안보고 풀어보기
         // 완전탐색으로 문제풀기
-        int val1=0;
-        int val2=0;
-        for(int i=0;i<wires.length;i++){
-            val1=relationCnt(i,wires);
-            val2=n-val1;
-            int total=Math.abs(val1-val2);
-            answer=Math.min(total,answer);
-        }
+        int a1 = 0;
+        int a2 = 0;
+        for (int i = 0; i < wires.length; i++) {
+			a1 = relationCnt(i, wires);
+			a2 = n - a1;
+			int total = Math.abs(a1 - a2);
+			answer = Math.min(total, answer);
+		}
         return answer;
     }
 	
 	public static int relationCnt (int idx, int[][] wires){
         
 		// BFS 탐색
-        Queue<Integer> que = new LinkedList<>();
-        List<int[]> list = new ArrayList<>();
-        for(int[] data:wires){
-            list.add(data);
-        }
-        
-        int parent=list.get(idx)[0];
-        que.add(parent);
-        list.remove(idx);
-        int cnt=0;
-        
-        while(!que.isEmpty()){
-          int temp=que.remove();           
-          for(int i=0;i<list.size();i++){
-              int[] arr= list.get(i);
-              boolean flag=true;
-              if(arr[1]==temp){
-                  que.add(arr[0]);
-                  list.remove(arr);
-                  flag=false;
-              }
-              if(arr[0]==temp){
-                  que.add(arr[1]);     
-                  list.remove(arr);  
-                  flag=false;
-              }   
-             if(flag==false){
-                 i--;
-             }
-          }
-          cnt++;
-        }
-        return cnt;
+		Queue<Integer> queue = new LinkedList<>();
+		List<int[]> list = new ArrayList<>();
+		for (int[] data : wires) {
+			list.add(data);
+		}
+		
+		int parent = list.get(idx)[0];
+		queue.add(parent);
+		list.remove(idx);
+		int cnt = 0;
+		
+	      while(!queue.isEmpty()){
+	        int temp=queue.remove();           
+	        for(int i=0;i<list.size();i++){
+	            int[] arr= list.get(i);
+	            boolean flag=true;
+	            if(arr[1]==temp){
+	            	queue.add(arr[0]);
+	                list.remove(arr);
+	                flag=false;
+	            }
+	            if(arr[0]==temp){
+	            	queue.add(arr[1]);     
+	                list.remove(arr);  
+	                flag=false;
+	            }   
+	           if(flag==false){
+	               i--;
+	           }
+	        }
+	        cnt++;
+	      }
+	      return cnt;
     }
 	
 }
