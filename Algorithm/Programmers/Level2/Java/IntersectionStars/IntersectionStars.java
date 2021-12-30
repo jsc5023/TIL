b35280b4ca912b51786c51b90b39c83924bd6db8
JungSeongCheol <kir5121@naver.com>
2021-12-30 23:41:13 +0900

    교점에 별만들기

--- Algorithm/Programmers/Level2/Java/IntersectionStars/IntersectionStars.java
+++ Algorithm/Programmers/Level2/Java/IntersectionStars/IntersectionStars.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class IntersectionStars {
 
 	static ArrayList<int[]> list;
+	static int max_x = Integer.MIN_VALUE;
+	static int min_x = Integer.MAX_VALUE;
+	static int max_y = Integer.MIN_VALUE;
+	static int min_y = Integer.MAX_VALUE;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] line = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
@@ -22,8 +27,31 @@ public class IntersectionStars {
         	
 		}
         
-        // 이제 어떻게 ...을 만들것인지 고민
-        String[] str = new String[];
+        boolean[][] answerTemp = new boolean[(int) (max_y- min_y +1)][(int) (max_x - min_x + 1)];
+
+        for (int[] ints : list) {
+            int x = (int) (ints[0] - min_x);
+            int y = (int) (ints[1] - max_y);
+
+            answerTemp[Math.abs(y)][Math.abs(x)] = true;
+        }
+
+        answer = new String[answerTemp.length];
+
+        int i = 0;
+        for (boolean[] bs : answerTemp) {
+            StringBuilder sb = new StringBuilder();
+            for (boolean b : bs) {
+                if(b){
+                    sb.append("*");
+                }else{
+                    sb.append(".");
+                }
+            }
+            answer[i] = sb.toString();
+            i++;
+        }
+        
         return answer;
     }
 	
@@ -41,10 +69,15 @@ public class IntersectionStars {
 		boolean check_y = doubleIntCheck(ans_y);
 		
 		if(check_x && check_y) {
+			max_x = Math.max(max_x, (int)ans_x);
+			min_x = Math.min(min_x, (int)ans_x);
+			max_y = Math.max(max_y, (int)ans_x);
+			min_y = Math.min(min_y, (int)ans_x);
+			
 			int[] arr = {(int)ans_x, (int)ans_y};
 			list.add(arr);
 		}
-			
+
 	}
 	
 	public static boolean doubleIntCheck(double number) {
