224494091c5baea7849422d1f347dd7f08848e6d
JungSeongCheol <kir5121@naver.com>
2022-01-01 23:12:57 +0900

    피로도 문제(아직 감안잡힘, 다시풀이필요)

---
+++ Algorithm/Programmers/Level2/Java/Fatigue_Level/Fatigue_Level.java
@@ -0,0 +1,22 @@
+
+public class Fatigue_Level {
+
+	static int nowFatigue;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 80;
+		int[][] dungeons = {{80,20},{50,40},{30,10}};
+		System.out.print(solution(k, dungeons));
+	}
+	
+	public static int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        
+        nowFatigue = k;
+        for (int i = 0; i < dungeons.length; i++) {
+			
+		}
+        return answer;
+    }
+
+}
