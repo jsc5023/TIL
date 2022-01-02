83f6387331e50a6b8f0b353acf444f7151604926
JungSeongCheol <kir5121@naver.com>
2022-01-02 23:34:50 +0900

    Java 피로도 문제풀이

--- Algorithm/Programmers/Level2/Java/Fatigue_Level/Fatigue_Level.java
+++ Algorithm/Programmers/Level2/Java/Fatigue_Level/Fatigue_Level.java
@@ -2,6 +2,8 @@
 public class Fatigue_Level {
 
 	static int nowFatigue;
+	static int answer = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int k = 80;
@@ -10,13 +12,30 @@ public class Fatigue_Level {
 	}
 	
 	public static int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        
         nowFatigue = k;
-        for (int i = 0; i < dungeons.length; i++) {
-			
-		}
+        
+        // dfs 로 문제를 풀어야됩니다.(경로를 찾아야되므로)
+        dfs(k, dungeons, new boolean[dungeons.length], 0);
+        
         return answer;
+    }
+	
+	 public static void dfs(int k, int[][] dungeons, boolean[] visit, int count) {
+        if(count>=visit.length) {
+            answer = count;
+            return;
+        }
+        for(int i=0 ; i<dungeons.length ; i++) {
+            if(visit[i]) continue;
+            if(k<dungeons[i][0]) {
+            	answer = Math.max(answer, count);
+                continue;
+            }
+            visit[i] = true;
+            dfs(k-dungeons[i][1], dungeons, visit, count+1);
+            visit[i] = false;
+        }
+        return;
     }
 
 }
