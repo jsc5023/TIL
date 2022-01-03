f0c93591e7b67d53c2eead0c1bddcb43f552932f
JungSeongCheol <kir5121@naver.com>
2022-01-03 23:04:40 +0900

    조이스틱 문제(아직 감도 못잡겠음)

---
+++ Algorithm/Programmers/Level2/Java/Joystick/Joystick.java
@@ -0,0 +1,19 @@
+
+public class Joystick {
+
+	static String nowAlpha = "A";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "JAN";
+		System.out.print(solution(name));
+	}
+	
+	 public static int solution(String name) {
+        int answer = 0;
+        
+        // 풀이방법 재생각
+        
+        return answer;
+    }
+
+}
