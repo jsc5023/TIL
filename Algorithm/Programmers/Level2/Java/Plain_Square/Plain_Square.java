ed8528f54c5fda804e3d7be4586fa7d58f35602e
JungSeongCheol <kir5121@naver.com>
2022-01-07 23:41:46 +0900

    멀쩡한 사각형 문제풀이(미완성, 푸는방법 다시 생각하기)

---
+++ Algorithm/Programmers/Level2/Java/Plain_Square/Plain_Square.java
@@ -0,0 +1,24 @@
+
+public class Plain_Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int w = 8;
+		int h = 12;
+		System.out.print(solution(w, h));
+		
+	}
+
+	public static long solution(int w, int h) {
+        long answer = 1;
+        // 어떤식으로 자를 수 있을지 고민해보기
+        
+        // 패턴을 잘모르겠음, 좀더 고민해보기
+        // 적어가면서 풀기
+        
+        long notCuttingSquare = w * h;
+        
+        
+        return answer;
+    }
+}
