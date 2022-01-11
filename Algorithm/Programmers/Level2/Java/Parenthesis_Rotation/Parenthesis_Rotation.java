3f29c7c1838f070bd5d645b9de112ddc6ab8bf1e
JungSeongCheol <kir5121@naver.com>
2022-01-11 23:32:46 +0900

    괄호 회전하기 문제풀이

--- Algorithm/Programmers/Level2/Java/Parenthesis_Rotation/Parenthesis_Rotation.java
+++ Algorithm/Programmers/Level2/Java/Parenthesis_Rotation/Parenthesis_Rotation.java
@@ -12,8 +12,6 @@ public class Parenthesis_Rotation {
 	public static int solution(String s) {
         int answer = 0;
         
-        Set<String> hs = new HashSet<>();
-        
         String nowStr = s;
         for (int i = 0; i < s.length(); i++) {
 			nowStr = nowStr.substring(1) + nowStr.substring(0, 1);
@@ -24,6 +22,18 @@ public class Parenthesis_Rotation {
     }
 	
 	public static boolean isCorrectParenthesis(String nowStr) {
+		
+		Set<Character> hs = new HashSet<>();
+		char[] charArr = nowStr.toCharArray();
+		for (int i = 0; i < charArr.length; i++) {
+			if(charArr[i] == '[')
+				hs.add('[');
+			if(charArr[i] == '(')
+				hs.add('(');
+			if(charArr[i] == '{')
+				hs.add('{');
+		}
+		if(nowStr.)
 		
 		return true;
 	}
