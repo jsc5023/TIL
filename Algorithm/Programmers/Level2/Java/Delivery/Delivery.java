47309a6637297000b1d3bf7e50c0bcc84de83d4b
JungSeongCheol <kir5121@naver.com>
2022-01-24 12:47:14 +0900

    부등호 등의 오류가 많아서 재풀이

--- Algorithm/Programmers/Level2/Java/Delivery/Delivery.java
+++ Algorithm/Programmers/Level2/Java/Delivery/Delivery.java
@@ -17,18 +17,18 @@ public class Delivery {
         // https://geehye.github.io/programmers-ets-01/# 참조
         int[][] arr = new int[N+1][N+1];
         
-        for (int i = 1; i < N; i++) {
-			for (int j = 1; j < N; j++) {
+        for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
 				if(i == j)
 					arr[i][j] = 0;
 				else
-					arr[i][j] = 500000;
+					arr[i][j] = 500001;
 			}
 		}
         // 여기까진 쉽게이해함, 
         
         for (int i = 0; i < road.length; i++)  {
-        	if(arr[road[i][0]][road[i][1]] > road[i][2])
+        	if(arr[road[i][0]][road[i][1]] < road[i][2])
         		continue;// 더 시간이 적게드는 길로 가야되기 때문에, 더 큰값은 필요없음
         	
         	arr[road[i][0]][road[i][1]] = road[i][2];
@@ -36,8 +36,8 @@ public class Delivery {
         }
         
         for (int i = 1; i <= N; i++) {
-			for (int j = 0; j <= N; j++) {
-				for (int j2 = 0; j2 < N; j2++) {
+			for (int j = 1; j <= N; j++) {
+				for (int j2 = 1; j2 <= N; j2++) {
 					if(j2 == j)
 						continue;
 					
@@ -48,7 +48,8 @@ public class Delivery {
 		}
 
         for (int i = 1; i <= N; i++) {
-            if (arr[1][i] <= K) answer++;
+            if (arr[1][i] <= K)
+            	answer++;
         }
         
         return answer;
