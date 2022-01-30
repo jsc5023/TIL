340eebf6b64c40349a015dcf8a27163a9e6fef59
JungSeongCheol <kir5121@naver.com>
2022-01-30 23:35:14 +0900

    오픈 체팅방 문제풀이

--- Algorithm/Programmers/Level2/Java/OpenChatRoom/OpenChatRoom.java
+++ Algorithm/Programmers/Level2/Java/OpenChatRoom/OpenChatRoom.java
@@ -1,4 +1,5 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class OpenChatRoom {
 
@@ -9,14 +10,32 @@ public class OpenChatRoom {
 	}
 
 	public static String[] solution(String[] record) {
-        String[] answer = {};
-        
-        // 문제 접근법
-        // Enter를 Replace한후, Hash에 저장, 그값을 변경한다.
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < record.length; i++) {
-        	
+		ArrayList<String> arr = new ArrayList<>();
+		HashMap<String, String> map = new HashMap<>();
+		for (int i = 0; i < record.length; i++) {
+			String[] cmd = record[i].split(" ");
+			
+			if(cmd[0].equals("Enter")) {
+				arr.add(cmd[1] + "님이 들어왔습니다.");
+				map.put(cmd[1], cmd[2]);
+			}
+			else if(cmd[0].equals("Change")) {
+				map.put(cmd[1], cmd[2]);
+			}
+			else if(cmd[0].equals("Leave")) {
+				arr.add(cmd[1] + "님이 나갔습니다.");
+			}
 		}
-        return answer;
+		
+		String[] answer = new String[arr.size()];
+		
+		for (int i = 0; i < arr.size(); i++) {
+			int idx = arr.get(i).indexOf("님");
+			String id = arr.get(i).substring(0, idx);
+			String[] tmp = arr.get(i).split(" ");
+			answer[i] = map.get(id) + "님이 " + temp[1];
+		}
+		
+		return answer;
     }
 }
