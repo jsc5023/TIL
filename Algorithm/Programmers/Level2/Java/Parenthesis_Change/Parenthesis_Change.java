91dfe82bf87ff295e5c85c8335089114e95d306f
JungSeongCheol <kir5121@naver.com>
2022-02-01 22:58:23 +0900

    자료 문제풀이

--- Algorithm/Programmers/Level2/Java/Parenthesis_Change/Parenthesis_Change.java
+++ Algorithm/Programmers/Level2/Java/Parenthesis_Change/Parenthesis_Change.java
@@ -4,14 +4,17 @@ public class Parenthesis_Change {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String p = "(()())()";
-		System.out.print(Solution(p));
+		String p = ")(";
+		System.out.print(solution(p));
 	}
 
 	public static String solution(String p) {
         String answer = "";
         
         Stack<Character> stack = new Stack<>();
+        
+        String[] arrStr = new String[p.length()];
+        
         if(p.isBlank())
         	return "";
         else {
@@ -20,13 +23,15 @@ public class Parenthesis_Change {
         			stack.add(p.charAt(i));
     			else {
     				Character nowChar = stack.peek();
-    				
     				if(nowChar == '(') {
     					stack.pop();
     				}
     			}
     				
 			}
+        	
+        	if(stack.isEmpty())
+        		return p;
         	
         }
         return answer;
