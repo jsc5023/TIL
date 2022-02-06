1838cd4fd13fe1901ba50b97eb07f1e19541a309
JungSeongCheol <kir5121@naver.com>
2022-02-06 23:32:34 +0900

    수식 최대화

--- Algorithm/Programmers/Level2/Java/MaxFormulas/MaxFormulas.java
+++ Algorithm/Programmers/Level2/Java/MaxFormulas/MaxFormulas.java
@@ -1,38 +1,85 @@
+import java.util.ArrayList;
 
 public class MaxFormulas {
 
+	static char[] prior = {'+', '-', '*'};
+    static boolean[] check = new boolean[3];
+    static ArrayList<Long> nums = new ArrayList<Long>();
+    static ArrayList<Character> ops = new ArrayList<Character>();
+    static long answer;
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 참조 https://asong-study-record.tistory.com/83 참조
+		// 혼자못풀어서, 참조함, 주석 및 재풀이 필요
 		String expression = "100-200*300-500+20";
 		System.out.print(solution(expression));
 	}
 	
 	public static long solution(String expression) {
-        long answer = Long.MIN_VALUE;
-        // 우선순위정하기
-        // 1. + , - , *
-        // 2. + , * , -
-        // 3. *, +, -
-        // 4. *, -, +
-        // 5. -, * , +
-        // 6. -, +, *
-        
-        for (int i = 0; i < expression.length(); i++) {
-			answer = findExpression(i, expression, answer);
-		}
-        
+		answer = 0;
+
+        String num="";
+        for(int i=0;i<expression.length();i++){
+            if(expression.charAt(i) >= '0' && expression.charAt(i) <= '9'){
+            	// 숫자일 경우 num에더함
+                num += expression.charAt(i);
+            }
+            else{
+                nums.add(Long.parseLong(num));
+                num = "";
+                ops.add(expression.charAt(i));
+            }
+        }
+        nums.add(Long.parseLong(num));
+        dfs(0, new char[3]);
+
         return answer;
     }
 	
-	public static long findExpression(int i, String expression, long answer) {
-		
-		if(i == 0) {
-			String[] plusString = expression.split("+");
-			
-			
-		}
-		
-		return 0;
+	 public static void dfs(int count, char[] p){
+        if(count == 3){
+            ArrayList<Long> cNums = new ArrayList<>(nums);
+            ArrayList<Character> cOps = new ArrayList<Character>(ops);
+
+            for(int i=0;i<p.length;i++){
+                for(int j=0; j< cOps.size(); j++){
+                    if(p[i] == cOps.get(j)){
+                        Long res = findExpression(cNums.remove(j), cNums.remove(j), p[i]);
+                        cNums.add(j, res);
+                        cOps.remove(j);
+                        j--;
+                    }
+                }
+            }
+            answer = Math.max(answer, Math.abs(cNums.get(0)));
+            return;
+
+        }
+
+        for(int i=0; i< 3; i++){
+            if(!check[i]){
+                check[i] = true;
+                p[count] = prior[i];
+                dfs(count+1,p);
+                check[i] = false;
+            }
+        }
+    }
+	 
+	 public static long findExpression(Long num1, Long num2, char op) {
+		long num = 0;
+        switch (op){
+            case '+' : {
+                return num1 + num2;
+            }
+            case '-' : {
+                return num1 - num2;
+            }
+            case '*' : {
+                return num1 * num2;
+            }
+        }
+        return num;
 	}
 
 }
