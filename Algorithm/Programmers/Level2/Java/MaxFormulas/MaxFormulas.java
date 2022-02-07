6f3f55d02ce0e5fed6ef2783f22187aaa4794fd8
JungSeongCheol <kir5121@naver.com>
2022-02-07 22:36:57 +0900

    수식 최대값 문제풀이 완료(재풀이, 변경)

--- Algorithm/Programmers/Level2/Java/MaxFormulas/MaxFormulas.java
+++ Algorithm/Programmers/Level2/Java/MaxFormulas/MaxFormulas.java
@@ -8,6 +8,8 @@ public class MaxFormulas {
     static ArrayList<Character> ops = new ArrayList<Character>();
     static long answer;
     
+    static char[] operator = {'+', '-', '*'};
+    
 	public static void main(String[] args) {
 		// 참조 https://asong-study-record.tistory.com/83 참조
 		// 혼자못풀어서, 참조함, 주석 및 재풀이 필요
@@ -18,33 +20,33 @@ public class MaxFormulas {
 	public static long solution(String expression) {
 		answer = 0;
 
-        String num="";
-        for(int i=0;i<expression.length();i++){
-            if(expression.charAt(i) >= '0' && expression.charAt(i) <= '9'){
-            	// 숫자일 경우 num에더함
-                num += expression.charAt(i);
-            }
-            else{
-                nums.add(Long.parseLong(num));
-                num = "";
-                ops.add(expression.charAt(i));
-            }
-        }
-        nums.add(Long.parseLong(num));
-        dfs(0, new char[3]);
-
+		String num = "";
+		for (int i = 0; i < expression.length(); i++) {
+			char nowChar = expression.charAt(i);
+			if(nowChar >= '0' && nowChar <= '9') {
+				num += nowChar;
+			}
+			else {
+				nums.add(Long.parseLong(num));
+				num = "";
+				ops.add(expression.charAt(i));
+			}
+		}
+		nums.add(Long.parseLong(num));
+		dfs(0, new char[3]);
+		
         return answer;
     }
 	
-	 public static void dfs(int count, char[] p){
+	 public static void dfs(int count, char[] op){
         if(count == 3){
             ArrayList<Long> cNums = new ArrayList<>(nums);
             ArrayList<Character> cOps = new ArrayList<Character>(ops);
 
-            for(int i=0;i<p.length;i++){
+            for(int i=0;i<op.length;i++){
                 for(int j=0; j< cOps.size(); j++){
-                    if(p[i] == cOps.get(j)){
-                        Long res = findExpression(cNums.remove(j), cNums.remove(j), p[i]);
+                    if(op[i] == cOps.get(j)){
+                        Long res = findExpression(cNums.remove(j), cNums.remove(j), op[i]);
                         cNums.add(j, res);
                         cOps.remove(j);
                         j--;
@@ -55,15 +57,15 @@ public class MaxFormulas {
             return;
 
         }
-
-        for(int i=0; i< 3; i++){
-            if(!check[i]){
-                check[i] = true;
-                p[count] = prior[i];
-                dfs(count+1,p);
-                check[i] = false;
-            }
-        }
+        
+        for (int i = 0; i < op.length; i++) {
+			if(!check[i]) {
+				check[i] = true;
+				op[count] = prior[i];// 연산자
+				dfs(count + 1, op);
+				check[i] = false;
+			}
+		}
     }
 	 
 	 public static long findExpression(Long num1, Long num2, char op) {
