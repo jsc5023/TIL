9c29619626977d95313281929281fd958d7b59f3
JungSeongCheol <kir5121@naver.com>
2022-02-12 23:49:29 +0900

    순위검색

--- Algorithm/Programmers/Level2/Java/SearchRank/SearchRank.java
+++ Algorithm/Programmers/Level2/Java/SearchRank/SearchRank.java
@@ -1,63 +1,56 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class SearchRank {
 
+	static HashMap<String, ArrayList<Integer>> map;
+	
 	public static void main(String[] args) {
+		// https://velog.io/@jh5253/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%88%9C%EC%9C%84%EA%B2%80%EC%83%89-Java%EC%9E%90%EB%B0%94 참조
 		String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
 		String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
 		solution(info, query);
 	}
 	
-	public static int[] solution(String[] info, String[] query) {
-       
-		// 풀이 변경(이분탐색으로 풀어야합니다.)
-		
-		//Binary Search
-        List<UserInfo> userList = new ArrayList<>();
-        
-        for (int i = 0; i < info.length; i++) {
-
-        	String[] userinfo = info[i].split(" ");
-        	String codingTestProgram = userinfo[0];
-        	String jobGroup = userinfo[1];
-        	String careerGroup = userinfo[2];
-        	String favoritFood = userinfo[3];
-        	int score = Integer.parseInt(userinfo[4]);
-        	UserInfo user = new UserInfo(codingTestProgram, jobGroup, careerGroup, favoritFood, score);
-        	userList.add(user);
-        	
-		}
-        
+	
+    public static int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
+        map = new HashMap<>();
+        for(String item : info)
+        {
+            String [] infoArr = item.split(" ");
+            comb("",0,infoArr);
+        }
         
-        
-        for (int i = 0; i < query.length; i++) {
-        	String[] queryInfo = query[i].split(" and ");
-        	String codingTestProgram = queryInfo[0];
-        	String jobGroup = queryInfo[1];
-        	String careerGroup = queryInfo[2];
-        	String[] favoritFood_Score = queryInfo[3].split(" ");
-        	String favoritFood = favoritFood_Score[0];
-        	int score = Integer.parseInt(favoritFood_Score[1]);
-        	int index = 0;
-        	for (UserInfo user : userList) {
-				if((codingTestProgram.equals("-") || user.codingTestProgram.equals(codingTestProgram))
-						&& (jobGroup.equals("-") || user.jobGroup.equals(jobGroup))
-						&& (careerGroup.equals("-") || user.careerGroup.equals(careerGroup))
-						&& (favoritFood.equals("-") || user.favoritFood.equals(favoritFood))
-						&& (user.score >= score)) {
-					index++;
-				}
-			}
-        	answer[i] = index;
-		}
-        
-        
+        int idx = 0;
+        for(String q : query)
+        {
+            String str = q.replace(" and ","");
+            String [] tmp = str.split(" ");
+            Collections.sort(map.get(tmp[0]));
+            answer[idx++] = binarySearch(tmp[0],Integer.parseInt(tmp[1]));
+        }
         return answer;
     }
-	
-	static int binarySearch(String query, int score)
+    
+    static void comb(String str,int depth,String [] arr) {
+        if (depth == 4) {
+            int score = Integer.parseInt(arr[4]);
+            if (map.containsKey(str)) map.get(str).add(score);
+            else {
+                ArrayList<Integer> tmp = new ArrayList<>();
+                tmp.add(score);
+                map.put(str, tmp);
+            }
+            return;
+        }
+
+        comb(str + "-", depth + 1, arr);
+        comb(str + arr[depth], depth + 1, arr);
+    }
+
+    static int binarySearch(String query, int score)
     {
         if(!map.containsKey(query)) return 0;
         ArrayList<Integer> tmpList = map.get(query);
@@ -71,22 +64,4 @@ public class SearchRank {
         }
         return tmpList.size() - start;
     }
-	
-	
-	public static class UserInfo{
-		String codingTestProgram;
-		String jobGroup;
-		String careerGroup;
-		String favoritFood;
-		int score; 
-		
-		public UserInfo(String codingTestProgram, String jobGroup, String careerGroup, String favoritFood, int score) {
-			this.codingTestProgram = codingTestProgram;
-			this.jobGroup = jobGroup;
-			this.careerGroup = careerGroup;
-			this.favoritFood = favoritFood;
-			this.score = score;
-		}
-	}
-
 }
