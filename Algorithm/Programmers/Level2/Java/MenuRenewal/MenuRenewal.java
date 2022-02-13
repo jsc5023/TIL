f3dd4a7492f1b7586a09fe0d1e3f8ca02a0cbc4b
JungSeongCheol <kir5121@naver.com>
2022-02-13 23:38:44 +0900

    예제

---
+++ Algorithm/Programmers/Level2/Java/MenuRenewal/MenuRenewal.java
@@ -0,0 +1,16 @@
+
+public class MenuRenewal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+		int[] course = {2,3,4};
+		System.out.print(solution(orders, course));
+	}
+	
+	 public static String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+
+}
