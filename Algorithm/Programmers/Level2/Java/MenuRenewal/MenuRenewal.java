bc109cf6c291bee7ab5dc627d22934bae096d659
JungSeongCheol <kir5121@naver.com>
2022-02-14 23:53:00 +0900

    매뉴 리뉴얼(미완성)

--- Algorithm/Programmers/Level2/Java/MenuRenewal/MenuRenewal.java
+++ Algorithm/Programmers/Level2/Java/MenuRenewal/MenuRenewal.java
@@ -1,15 +1,27 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class MenuRenewal {
 
+	public static HashMap<String, Integer> map;
+	static int max;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// https://gwang920.github.io/algorithm/progreammers-1-72411/ ÂüÁ¶
 		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
 		int[] course = {2,3,4};
 		System.out.print(solution(orders, course));
 	}
 	
-	 public static String[] solution(String[] orders, int[] course) {
+	public static String[] solution(String[] orders, int[] course) {
         String[] answer = {};
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        
+        for (int i = 0; i < course.length; i++) {
+        	map = new HashMap<>();
+        	max = 0;
+        	
+        }
+	
         return answer;
     }
 
