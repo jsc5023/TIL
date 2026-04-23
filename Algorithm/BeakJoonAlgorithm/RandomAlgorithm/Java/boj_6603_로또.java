3a2eb866d80c76e213cf76c2474966f5b47d626c
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2026-04-23 18:07:06 +0900

    로또 문제 풀이

---
+++ Algorithm/BeakJoonAlgorithm/RandomAlgorithm/Java/boj_6603_로또.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_6603_로또 {
+    static FastReader scan = new FastReader();
+    static int N;
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    private static void input() {
+        N = scan.nextInt();
+    }
+
+
+    private static void pro() {
+
+        int sugarBoxes = 0;
+
+        while(N > 0) {
+            if(N % 5 == 0) {
+                sugarBoxes += (N / 5);
+                N = 0;
+                break;
+            }
+            sugarBoxes++;
+            N -= 3;
+        }
+
+        if(N != 0) {
+            sugarBoxes = -1; // 불가능 -1
+        }
+
+        System.out.println(sugarBoxes);
+    }
+
+
+
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
